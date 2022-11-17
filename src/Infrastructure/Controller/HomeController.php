a1cba19bc2b40f13a0aa3514aa3445ab0291411b
florimondmanca <florimond.manca@fairness.coop>
2022-11-17 15:30:19 +0100

    Setup translations

--- src/Infrastructure/Controller/HomeController.php
+++ src/Infrastructure/Controller/HomeController.php
@@ -6,17 +6,22 @@ namespace App\Infrastructure\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class HomeController
 {
     public function __construct(
         private \Twig\Environment $twig,
+        private TranslatorInterface $translator,
     ) {
     }
 
     #[Route('/', name: 'app_home', methods: 'GET')]
     public function __invoke(): Response
     {
-        return new Response($this->twig->render('index.html.twig'));
+        $title = $this->translator->trans('home.title');
+        $context = ['title' => $title];
+
+        return new Response($this->twig->render('index.html.twig', $context));
     }
 }
