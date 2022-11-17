6d00a28ee3ec0bc2a7b705c5d703f4720f45baa4
florimondmanca <florimond.manca@fairness.coop>
2022-11-17 16:07:26 +0100

    Use xliff

--- src/Infrastructure/Controller/HomeController.php
+++ src/Infrastructure/Controller/HomeController.php
@@ -6,22 +6,17 @@ namespace App\Infrastructure\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class HomeController
 {
     public function __construct(
         private \Twig\Environment $twig,
-        private TranslatorInterface $translator,
     ) {
     }
 
     #[Route('/', name: 'app_home', methods: 'GET')]
     public function __invoke(): Response
     {
-        $title = $this->translator->trans('home.title');
-        $context = ['title' => $title];
-
-        return new Response($this->twig->render('index.html.twig', $context));
+        return new Response($this->twig->render('index.html.twig'));
     }
 }
