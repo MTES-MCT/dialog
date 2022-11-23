39244cedb9c8ecfa165673968f63f6ea36fce7d1
mmarchois <mathieu.marchois@gmail.com>
2022-11-23 17:27:10 +0100

    Add ConditionSet model

---
+++ src/Domain/TrafficRegulation/Condition/ConditionSet.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\TrafficRegulation\Condition;
+
+use App\Domain\TrafficRegulation\Condition\Enum\OperatorEnum;
+use App\Domain\TrafficRegulation\RegulationCondition;
+
+class ConditionSet
+{
+    /** @var RegulationCondition[] */
+    private array $conditions = [];
+
+    public function __construct(
+        private string $uuid,
+        private OperatorEnum $operator,
+    ) {
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getOperator(): OperatorEnum
+    {
+        return $this->operator;
+    }
+
+    public function addCondition(RegulationCondition $regulationCondition): void
+    {
+        if (\in_array($regulationCondition, $this->conditions, true)) {
+            return;
+        }
+
+        array_push($this->conditions, $regulationCondition);
+    }
+
+    public function getConditions(): array
+    {
+        return $this->conditions;
+    }
+}
