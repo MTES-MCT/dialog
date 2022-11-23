36041b48a89e21f58fe17b8b4b25aaecee10a80f
mmarchois <mathieu.marchois@gmail.com>
2022-11-23 09:24:32 +0100

    Add Symfony Messenger & Validator components to init CQRS

---
+++ src/Infrastructure/Adapter/QueryBus.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Adapter;
+
+use App\Application\QueryBusInterface;
+use App\Application\QueryInterface;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class QueryBus implements QueryBusInterface
+{
+    use HandleTrait {
+        handle as doHandle;
+    }
+
+    private MessageBusInterface $queryBus;
+
+    public function __construct(MessageBusInterface $queryBus)
+    {
+        $this->messageBus = $queryBus;
+    }
+
+    public function handle(QueryInterface $query): mixed
+    {
+        try {
+            return $this->doHandle($query);
+        } catch (HandlerFailedException $exception) {
+            $previous = $exception->getPrevious();
+
+            if ($previous === null) {
+                throw $exception;
+            }
+
+            throw $previous;
+        }
+    }
+}
