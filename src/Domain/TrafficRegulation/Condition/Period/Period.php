b044b91d0935b2f127434bc6f57a9c184bd8b2cb
mmarchois <mathieu.marchois@gmail.com>
2022-11-25 11:29:18 +0100

    Add Period model

---
+++ src/Domain/TrafficRegulation/Condition/Period/Period.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\TrafficRegulation\Condition\Period;
+
+class Period
+{
+    private ?OverallPeriod $overallValidPeriod = null;
+    private ?OverallPeriod $overallExceptionPeriod = null;
+
+    public function __construct(
+        private string $uuid,
+        private ?string $name = null,
+        private ?\DateTimeInterface $startDate = null,
+        private ?\DateTimeInterface $endDate = null,
+    ) {
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function getOverallValidPeriod(): ?OverallPeriod
+    {
+        return $this->overallValidPeriod;
+    }
+
+    public function getOverallExceptionPeriod(): ?OverallPeriod
+    {
+        return $this->overallExceptionPeriod;
+    }
+}
