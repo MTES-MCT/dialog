e606758c7475a4ecff8c0dce3d3d1154556b18ab
mmarchois <mathieu.marchois@gmail.com>
2022-11-25 15:33:17 +0100

    Add TimePeriodOfDay model

---
+++ src/Domain/TrafficRegulation/Condition/Period/TimePeriodOfDay.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\TrafficRegulation\Condition\Period;
+
+class TimePeriodOfDay
+{
+    public function __construct(
+        private string $uuid,
+        private \DateTimeImmutable $startTime,
+        private \DateTimeImmutable $endTime,
+        private Period $period,
+    ) {
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getStartTime(): \DateTimeImmutable
+    {
+        return $this->startTime;
+    }
+
+    public function getEndTime(): \DateTimeImmutable
+    {
+        return $this->endTime;
+    }
+
+    public function getPeriod(): Period
+    {
+        return $this->period;
+    }
+}
