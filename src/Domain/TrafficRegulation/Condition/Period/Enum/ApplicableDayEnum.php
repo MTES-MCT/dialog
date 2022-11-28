0db470f3967d91bc9bf19b7f34c89a1c13e5faeb
mmarchois <mathieu.marchois@gmail.com>
2022-11-28 11:36:08 +0100

    add dayweekmonth model

---
+++ src/Domain/TrafficRegulation/Condition/Period/Enum/ApplicableDayEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\TrafficRegulation\Condition\Period\Enum;
+
+// We use the DATEXII DayEnum values [docs/spec/datex2/DATEXII_3_Common.xsd]
+enum ApplicableDayEnum: string
+{
+    case MONDAY = 'monday';
+    case TUESDAY = 'tuesday';
+    case WEDNESDAY = 'wednesday';
+    case THURSDAY = 'thursday';
+    case FRIDAY = 'friday';
+    case SATURDAY = 'saturday';
+    case SUNDAY = 'sunday';
+}
