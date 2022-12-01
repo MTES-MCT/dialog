a54f4108678f1628382a03bfd97aa8d12be7666c
florimondmanca <florimond.manca@fairness.coop>
2022-12-01 13:30:12 +0100

    Add validation

--- src/Application/RegulationOrder/Command/CreateRegulationOrderCommandHandler.php
+++ src/Application/RegulationOrder/Command/CreateRegulationOrderCommandHandler.php
@@ -7,17 +7,26 @@ namespace App\Application\RegulationOrder\Command;
 use App\Application\IdFactoryInterface;
 use App\Domain\RegulationOrder\RegulationOrder;
 use App\Domain\RegulationOrder\Repository\RegulationOrderRepositoryInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class CreateRegulationOrderCommandHandler
 {
     public function __construct(
         private IdFactoryInterface $idFactory,
+        private ValidatorInterface $validator,
         private RegulationOrderRepositoryInterface $repository,
     ) {
     }
 
     public function __invoke(CreateRegulationOrderCommand $command): string
     {
+        $errors = $this->validator->validate($command);
+
+        if (\count($errors) > 0) {
+            throw new ValidationFailedException(null, $errors);
+        }
+
         $uuid = $this->idFactory->make();
         $description = $command->description;
         $issuingAuthority = $command->issuingAuthority;
--- tests/Unit/Application/RegulationOrder/CreateRegulationOrderTest.php
+++ tests/Unit/Application/RegulationOrder/CreateRegulationOrderTest.php
@@ -9,13 +9,18 @@ use App\Application\RegulationOrder\Command\CreateRegulationOrderCommand;
 use App\Application\RegulationOrder\Command\CreateRegulationOrderCommandHandler;
 use App\Domain\RegulationOrder\RegulationOrder;
 use App\Domain\RegulationOrder\Repository\RegulationOrderRepositoryInterface;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-final class CreateRegulationOrderTest extends TestCase
+final class CreateRegulationOrderTest extends KernelTestCase
 {
     public function testCreate(): void
     {
+        self::bootKernel();
+        $container = static::getContainer();
+
         $idFactory = $this->createMock(IdFactoryInterface::class);
+        $validator = $container->get(ValidatorInterface::class);
         $regulationOrderRepository = $this->createMock(RegulationOrderRepositoryInterface::class);
         $createdRegulationOrder = $this->createMock(RegulationOrder::class);
 
@@ -32,13 +37,39 @@ final class CreateRegulationOrderTest extends TestCase
             ->method('getUuid')
             ->willReturn('f331d768-ed8b-496d-81ce-b97008f338d0');
 
-        $handler = new CreateRegulationOrderCommandHandler($idFactory, $regulationOrderRepository);
+        $handler = new CreateRegulationOrderCommandHandler($idFactory, $validator, $regulationOrderRepository);
 
-        $uuid = $handler(new CreateRegulationOrderCommand(
-            'Interdiction de circuler',
-            'Ville de Paris',
-        ));
+        $uuid = $handler(
+            new CreateRegulationOrderCommand(
+                'Interdiction de circuler',
+                'Ville de Paris',
+            )
+        );
 
         $this->assertSame('f331d768-ed8b-496d-81ce-b97008f338d0', $uuid);
+    }
+
+    public function testCreateInvalid(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $idFactory = $container->get(IdFactoryInterface::class);
+        $validator = $container->get(ValidatorInterface::class);
+        $regulationOrderRepository = $this->createMock(RegulationOrderRepositoryInterface::class);
+
+        $regulationOrderRepository->expects(self::never())->method('save');
+
+        $handler = new CreateRegulationOrderCommandHandler($idFactory, $validator, $regulationOrderRepository);
+
+        $this->expectException('Symfony\Component\Validator\Exception\ValidationFailedException');
+
+        $handler(
+            new CreateRegulationOrderCommand(
+                '',
+                '',
+            )
+        );
+
     }
 }
