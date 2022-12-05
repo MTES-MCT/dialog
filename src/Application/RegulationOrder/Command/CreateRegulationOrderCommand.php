e2ee66fc223cda9d1822f79fde7c85b248fb621a
Florimond Manca <florimond.manca@fairness.coop>
2022-12-05 13:26:01 +0100

    Add CreateRegulationOrderCommand (#55)
    * Add CreateRegulationOrderCommand
    
    * Drop out of scope list and getById implementations
    
    * Add validation
    
    * Switch to public command attributes instead of constructor
    
    * Address feedback, drop validation test case

---
+++ src/Application/RegulationOrder/Command/CreateRegulationOrderCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\RegulationOrder\Command;
+
+use App\Application\CommandInterface;
+
+final class CreateRegulationOrderCommand implements CommandInterface
+{
+    public string $description;
+    public string $issuingAuthority;
+}
