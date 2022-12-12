df865040122d37c89266381f2bee82a058cc74d6
Florimond Manca <florimond.manca@fairness.coop>
2022-12-12 15:48:52 +0100

    Exclude /api from Basic auth (#68)
    * Exclude /api from Basic auth
    
    * Reconfigure basic-auth with Symfony
    
    * Make format

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
