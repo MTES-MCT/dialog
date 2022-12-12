78736dbeffdd7ec539007e30b633b60ee99d1933
mmarchois <mathieu.marchois@gmail.com>
2022-12-12 17:37:55 +0100

    Split into two queries to retrieve restrictions

--- src/Application/RegulationOrder/Query/GetRegulationOrdersToDatexFormatQueryHandler.php
+++ src/Application/RegulationOrder/Query/GetRegulationOrdersToDatexFormatQueryHandler.php
@@ -5,24 +5,25 @@ declare(strict_types=1);
 namespace App\Application\RegulationOrder\Query;
 
 use App\Application\RegulationOrder\View\PeriodView;
-use App\Application\RegulationOrder\View\RegulationOrderListItemView;
+use App\Application\RegulationOrder\View\RegulationOrderListForDatexFormatView;
 use App\Domain\RegulationOrder\Repository\RegulationOrderRepositoryInterface;
 
-final class GetAllRegulationOrderListItemsQueryHandler
+final class GetRegulationOrdersToDatexFormatQueryHandler
 {
     public function __construct(
         private RegulationOrderRepositoryInterface $repository,
     ) {
     }
 
-    public function __invoke(GetAllRegulationOrderListItemsQuery $query): array
+    public function __invoke(GetRegulationOrdersToDatexFormatQuery $query): array
     {
-        $regulationOrders = $this->repository->findRegulationOrders();
+        $regulationOrders = $this->repository->findRegulationOrdersForDatexFormat();
         $regulationOrderViews = [];
 
         foreach ($regulationOrders as $regulationOrder) {
-            $regulationOrderViews[] = new RegulationOrderListItemView(
+            $regulationOrderViews[] = new RegulationOrderListForDatexFormatView(
                 $regulationOrder['uuid'],
+                $regulationOrder['description'],
                 $regulationOrder['issuingAuthority'],
                 new PeriodView(
                     $regulationOrder['startPeriod'],
