65cb498f560c6efff48a7c05a54b0d35967ede31
mmarchois <mathieu.marchois@gmail.com>
2023-01-12 15:48:49 +0100

    List regulations

---
+++ src/Application/Regulation/Query/GetRegulationsQueryHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Regulation\Query;
+
+use App\Application\Regulation\View\PeriodView;
+use App\Application\Regulation\View\RegulationOrderListItemView;
+use App\Domain\Pagination;
+use App\Domain\Regulation\Enum\RegulationOrderRecordStatusEnum;
+use App\Domain\Regulation\Repository\RegulationOrderRecordRepositoryInterface;
+
+final class GetRegulationsQueryHandler
+{
+    public function __construct(
+        private RegulationOrderRecordRepositoryInterface $repository,
+    ) {
+    }
+
+    public function __invoke(GetRegulationsQuery $query): Pagination
+    {
+        $regulations = $this->repository->findRegulations($query->page, RegulationOrderRecordStatusEnum::DRAFT);
+        $totalItems = $this->repository->countRegulations(RegulationOrderRecordStatusEnum::DRAFT);
+        $regulationOrderViews = [];
+
+        foreach ($regulations as $regulation) {
+            $regulationOrderViews[] = new RegulationOrderListItemView(
+                $regulation['uuid'],
+                $regulation['status'],
+                $regulation['startPeriod'] ? new PeriodView(
+                    $regulation['startPeriod'],
+                    $regulation['endPeriod'],
+                ) : null,
+            );
+        }
+
+        return new Pagination($regulationOrderViews, $totalItems);
+    }
+}
--- src/Infrastructure/Controller/Regulation/ListRegulationsController.php
+++ src/Infrastructure/Controller/Regulation/ListRegulationsController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\Controller\Regulation;
 
 use App\Application\QueryBusInterface;
-use App\Application\Regulation\Query\GetAllRegulationOrderListItemsQuery;
+use App\Application\Regulation\Query\GetRegulationsQuery;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,15 +17,15 @@ final class ListRegulationsController
     ) {
     }
 
-    #[Route('/', name: 'app_regulations_list', methods: ['GET'])]
-    public function __invoke(): Response
+    #[Route('/{page}', name: 'app_regulations_list', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function __invoke(int $page = 1): Response
     {
-        $regulationOrders = $this->queryBus->handle(new GetAllRegulationOrderListItemsQuery());
+        $pagination = $this->queryBus->handle(new GetRegulationsQuery($page));
 
         return new Response($this->twig->render(
             name: 'regulation/index.html.twig',
             context: [
-                'regulationOrders' => $regulationOrders,
+                'pagination' => $pagination,
             ],
         ));
     }
