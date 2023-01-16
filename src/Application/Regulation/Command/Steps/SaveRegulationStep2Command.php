dc403339ecb52e6bdd2151297cf17a308d1114e8
Florimond Manca <florimond.manca@fairness.coop>
2023-01-16 17:42:02 +0100

    Add Location model and form (#84)
    * Add location form
    
    * Address feedback
    
    * Create postgis extension if not exist
    
    * Experiment: set enable_static_connection=false
    
    * Drop mapping_types
    
    * Drop CREATE EXTENSION IF NOT EXISTS postgis; from migration
    
    * Update Step2ControllerTest
    
    * Bring back CREATE EXTENSION IF NOT EXISTS for CI
    
    Co-authored-by: mmarchois < mathieu.marchois@gmail.com>

--- src/Application/Regulation/Command/Steps/SaveRegulationStep2Command.php
+++ src/Application/Regulation/Command/Steps/SaveRegulationStep2Command.php
@@ -5,12 +5,35 @@ declare(strict_types=1);
 namespace App\Application\Regulation\Command\Steps;
 
 use App\Application\CommandInterface;
+use App\Domain\Condition\Location;
 use App\Domain\Regulation\RegulationOrderRecord;
 
 final class SaveRegulationStep2Command implements CommandInterface
 {
+    public ?string $postalCode;
+    public ?string $city;
+    public ?string $roadName;
+    public ?string $fromHouseNumber;
+    public ?string $toHouseNumber;
+
     public function __construct(
         public readonly RegulationOrderRecord $regulationOrderRecord,
+        public readonly ?Location $location = null,
     ) {
+    }
+
+    public static function create(
+        RegulationOrderRecord $regulationOrderRecord,
+        Location $location = null,
+    ): self {
+        $command = new self($regulationOrderRecord, $location);
+
+        $command->postalCode = $location?->getPostalCode();
+        $command->city = $location?->getCity();
+        $command->roadName = $location?->getRoadName();
+        $command->fromHouseNumber = $location?->getFromHouseNumber();
+        $command->toHouseNumber = $location?->getToHouseNumber();
+
+        return $command;
     }
 }
---
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20230111170332.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230111170332 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE EXTENSION IF NOT EXISTS postgis;');
+        $this->addSql('CREATE TABLE location (uuid UUID NOT NULL, regulation_condition_uuid UUID NOT NULL, postal_code VARCHAR(5) NOT NULL, city VARCHAR(255) NOT NULL, road_name VARCHAR(60) NOT NULL, from_house_number VARCHAR(8) NOT NULL, from_point geometry(POINT, 2154) NOT NULL, to_house_number VARCHAR(8) NOT NULL, to_point geometry(POINT, 2154) NOT NULL, PRIMARY KEY(uuid))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_5E9E89CB9F073263 ON location (regulation_condition_uuid)');
+        $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CB9F073263 FOREIGN KEY (regulation_condition_uuid) REFERENCES regulation_condition (uuid) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE location DROP CONSTRAINT FK_5E9E89CB9F073263');
+        $this->addSql('DROP TABLE location');
+    }
+}
