412c4844e71c6a70eafbf06b0c0ecfe27f3c733c
Florimond Manca <florimond.manca@fairness.coop>
2023-01-30 14:32:09 +0100

    Move DSFR CSS imports to CSS bundle (#117)

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ Encore
     .setPublicPath('/build')
     .copyFiles({
         from: './node_modules/@gouvfr/dsfr/dist/',
+        pattern: /(dsfr.(no)?module.min.js)|(favicon\/)/,
         to: 'dsfr/[path][name].[ext]'
     })
 
