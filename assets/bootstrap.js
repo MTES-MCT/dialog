67da71c761789e720c137f60f501dc966d22fc6f
florimondmanca <florimond.manca@fairness.coop>
2023-02-13 13:53:21 +0100

    Partially address Turbo-related CSP errors

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,3 +1,4 @@
+import * as _Turbo from '@hotwired/turbo';
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
@@ -9,3 +10,13 @@ export const app = startStimulusApp(require.context(
 
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+
+// See: https://github.com/hotwired/turbo/issues/294#issuecomment-877842232
+document.addEventListener('turbo:before-fetch-request', (event) => {
+    event.detail.fetchOptions.headers['X-CSP-Nonce'] = document.querySelector('meta[name="csp-nonce"]').content;
+});
+document.addEventListener('turbo:before-cache', () => {
+    document.querySelectorAll('script[nonce]').forEach((element) => {
+        element.setAttribute('nonce', element.nonce);
+    });
+});
