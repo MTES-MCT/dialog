b4183ecee96e484030aa712588137e954b4a9eb5
Florimond Manca <florimond.manca@fairness.coop>
2023-03-15 16:43:40 +0100

    Fix formatting
    Co-authored-by: Mathieu MARCHOIS <mathieu.marchois@gmail.com>

--- tests/Integration/Infrastructure/Controller/Regulation/DuplicateRegulationControllerTest.php
+++ tests/Integration/Infrastructure/Controller/Regulation/DuplicateRegulationControllerTest.php
@@ -24,7 +24,8 @@ final class DuplicateRegulationControllerTest extends AbstractWebTestCase
         $this->assertSame('Copiée avec succès Vous pouvez modifier les informations que vous souhaitez dans cette copie de la réglementation.', $crawler->filter('div.fr-alert')->text());
     }
 
-    public function testDuplicateWithNoStartDateYet(): void {
+    public function testDuplicateWithNoStartDateYet(): void
+    {
         $client = $this->login('florimond.manca@beta.gouv.fr');
         $client->request('POST', '/regulations/867d2be6-0d80-41b5-b1ff-8452b30a95f5/duplicate', [
             'token' => $this->generateCsrfToken($client, 'duplicate-regulation'),
