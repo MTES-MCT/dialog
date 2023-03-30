24b32b8ae34212222a035ca759a1ff2856ed178f
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2023-03-30 10:21:45 +0200

    Change login parameters name to improve login security (#244)
    Co-authored-by: mmarchois < mathieu.marchois@gmail.com>

--- tests/Integration/Infrastructure/Controller/LoginControllerTest.php
+++ tests/Integration/Infrastructure/Controller/LoginControllerTest.php
@@ -18,8 +18,8 @@ final class LoginControllerTest extends AbstractWebTestCase
         $saveButton = $crawler->selectButton('Se connecter');
         $form = $saveButton->form();
 
-        $form["_username"] = "mathieu.marchois@beta.gouv.fr";
-        $form["_password"] = "password";
+        $form["email"] = "mathieu.marchois@beta.gouv.fr";
+        $form["password"] = "password";
         $client->submit($form);
         $this->assertResponseStatusCodeSame(302);
         $crawler = $client->followRedirect();
@@ -35,8 +35,8 @@ final class LoginControllerTest extends AbstractWebTestCase
 
         $saveButton = $crawler->selectButton('Se connecter');
         $form = $saveButton->form();
-        $form["_username"] = "mathieu@fairness.coop";
-        $form["_password"] = "password";
+        $form["email"] = "mathieu@fairness.coop";
+        $form["password"] = "password";
 
         $client->submit($form);
         $this->assertResponseStatusCodeSame(302);
