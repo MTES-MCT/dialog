d85e1bfde8d21c712f15bac8aa5e25734cb7f240
Florimond Manca <florimond.manca@fairness.coop>
2023-04-20 12:14:41 +0200

    Cleanup fragment controller queries (#283)
    * Fetch general info using specialized query
    
    * Add specialized GetRegulationForLocationQuery
    
    * Simplify: reuse existing queries

--- src/Infrastructure/Controller/Regulation/RegulationDetailController.php
+++ src/Infrastructure/Controller/Regulation/RegulationDetailController.php
@@ -7,25 +7,22 @@ namespace App\Infrastructure\Controller\Regulation;
 use App\Application\QueryBusInterface;
 use App\Application\Regulation\Query\GetRegulationOrderRecordSummaryQuery;
 use App\Application\Regulation\View\RegulationOrderRecordSummaryView;
-use App\Domain\Regulation\Exception\RegulationOrderRecordNotFoundException;
 use App\Domain\Regulation\Specification\CanOrganizationAccessToRegulation;
 use App\Domain\Regulation\Specification\CanRegulationOrderRecordBePublished;
-use App\Infrastructure\Security\SymfonyUser;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class RegulationDetailController
+final class RegulationDetailController extends AbstractRegulationController
 {
     public function __construct(
         private \Twig\Environment $twig,
-        private QueryBusInterface $queryBus,
-        private readonly CanOrganizationAccessToRegulation $canOrganizationAccessToRegulation,
-        private readonly CanRegulationOrderRecordBePublished $canRegulationOrderRecordBePublished,
-        private readonly Security $security,
+        protected QueryBusInterface $queryBus,
+        private CanRegulationOrderRecordBePublished $canRegulationOrderRecordBePublished,
+        CanOrganizationAccessToRegulation $canOrganizationAccessToRegulation,
+        Security $security,
     ) {
+        parent::__construct($queryBus, $security, $canOrganizationAccessToRegulation);
     }
 
     #[Route(
@@ -36,19 +33,10 @@ final class RegulationDetailController
     )]
     public function __invoke(string $uuid): Response
     {
-        try {
-            /** @var RegulationOrderRecordSummaryView */
-            $regulationOrderRecord = $this->queryBus->handle(new GetRegulationOrderRecordSummaryQuery($uuid));
-        } catch (RegulationOrderRecordNotFoundException) {
-            throw new NotFoundHttpException();
-        }
-
-        /** @var SymfonyUser */
-        $user = $this->security->getUser();
-
-        if (!$this->canOrganizationAccessToRegulation->isSatisfiedBy($regulationOrderRecord, $user->getOrganization())) {
-            throw new AccessDeniedHttpException();
-        }
+        /** @var RegulationOrderRecordSummaryView */
+        $regulationOrderRecord = $this->getRegulationOrderRecordUsing(function () use ($uuid) {
+            return $this->queryBus->handle(new GetRegulationOrderRecordSummaryQuery($uuid));
+        });
 
         return new Response(
             $this->twig->render(
