778f77da686eee3a3b92c75270cd96e45f95823a
florimondmanca <florimond.manca@fairness.coop>
2023-04-24 16:28:15 +0200

    Improve a11y statement

--- src/Infrastructure/Controller/AccessibilityStatementController.php
+++ src/Infrastructure/Controller/AccessibilityStatementController.php
@@ -8,17 +8,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class AccessibilityDeclarationController
+final class AccessibilityStatementController
 {
     public function __construct(
         private \Twig\Environment $twig,
     ) {
     }
 
-    #[Route('/accessibility', name: 'app_accessibility_declaration', methods: ['GET'])]
+    #[Route('/accessibility', name: 'app_accessibility_statement', methods: ['GET'])]
     public function __invoke(): Response
     {
-        $response = (new Response($this->twig->render('accessibility_declaration.html.twig')))
+        $response = (new Response($this->twig->render('accessibility_statement.html.twig')))
             ->setSharedMaxAge(86400);
         $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, 'true');
 
