18f5ac5a114cff455e3899e8fa9f73196b39d2a4
florimondmanca <florimond.manca@fairness.coop>
2023-05-02 17:54:23 +0200

    Address feedback

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,4 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { registerTurboEventHandlers } from './lib';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -10,28 +11,4 @@ export const app = startStimulusApp(require.context(
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
 
-// Register Turbo event handlers
-// See: https://turbo.hotwired.dev/reference/events
-
-document.addEventListener('turbo:frame-missing', (event) => {
-    _displayAnyDebugExceptionPage(event);
-});
-
-const _displayAnyDebugExceptionPage = (event) => {
-    /** @type {Response} */
-    const response = event.detail.response;
-
-    if (response.status !== 500 || !response.headers.has('X-Debug-Exception')) {
-        // Most likely not a Symfony debug error response.
-        return;
-    }
-
-    event.preventDefault();
-
-    response.text().then(html => {
-        document.open();
-        document.write(html);
-        document.close();
-        history.pushState({}, '', response.url);
-    });
-};
+registerTurboEventHandlers();
