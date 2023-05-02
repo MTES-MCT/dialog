a32b0a777c3cf9ffbf713e72a95dfcb4c83bb461
Florimond Manca <florimond.manca@fairness.coop>
2023-05-02 12:00:03 +0200

    Setup E2E tests using Playwright (#300)

---
+++ tests/e2e/list.spec.js
@@ -0,0 +1,50 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.use({ storageState: 'playwright/.auth/mathieu.json' });
+
+test.describe('Regulation list', () => {
+    test('Tabs', async ({ page }) => {
+        await page.goto('/regulations');
+
+        const temporaryTab = page.getByRole('tab', { name: 'Temporaires (3)' })
+        await temporaryTab.waitFor();
+        expect(temporaryTab).toHaveAttribute('aria-selected', 'true');
+        const permanentTab = page.getByRole('tab', { name: 'Permanents (1)' });
+        await permanentTab.waitFor();
+        expect(permanentTab).toHaveAttribute('aria-selected', 'false');
+
+        const temporaryTabPanel = page.getByRole('tabpanel', { name: 'Temporaires (3)' });
+        const permanentTabPanel = page.getByRole('tabpanel', { name: 'Permanents (1)' });
+        await expect(temporaryTabPanel).toBeVisible();
+        await expect(permanentTabPanel).not.toBeVisible();
+
+        await permanentTab.click();
+        expect(temporaryTab).toHaveAttribute('aria-selected', 'false');
+        await expect(temporaryTabPanel).not.toBeVisible();
+        expect(permanentTab).toHaveAttribute('aria-selected', 'true');
+        await expect(permanentTabPanel).toBeVisible();
+    });
+
+    test('Delete modal', async ({ page }) => {
+        await page.goto('/regulations');
+
+        const deleteModal = page.locator('[id="regulation-delete-modal"]');
+        await expect(deleteModal).not.toBeVisible();
+
+        const deleteBtn = page.getByRole('button', { name: "Supprimer l'arrêté FO1/2023", exact: true });
+        await deleteBtn.click();
+        await expect(deleteModal).toBeVisible();
+
+        // Don't delete
+        await deleteModal.getByRole('button', { name: 'Ne pas supprimer', exact: true }).click();
+        await expect(deleteModal).not.toBeVisible();
+        await expect(page.getByText('FO1/2023', { exact: true })).toBeVisible();
+
+        // Proceed to deletion
+        await deleteBtn.click();
+        await deleteModal.getByRole('button', { name: 'Supprimer', exact: true }).click();
+        await expect(deleteModal).not.toBeVisible();
+        await expect(page.getByText('FO1/2023', { exact: true })).not.toBeVisible();
+    });
+});
