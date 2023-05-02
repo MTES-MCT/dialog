1ca0329dd60f43bb90782a5825f6a50fa1a3d998
Florimond Manca <florimond.manca@fairness.coop>
2023-05-02 18:20:02 +0200

    Display Symfony error responses instead of "Content missing" (#310)
    * Display Symfony error responses instead of "Content missing"
    
    * Address feedback

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,4 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { registerTurboEventHandlers } from './lib';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -9,3 +10,5 @@ export const app = startStimulusApp(require.context(
 
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+
+registerTurboEventHandlers();
