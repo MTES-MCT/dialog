4e8b86f35ac5ab674928475a93b6c411ba1b733a
florimondmanca <florimond.manca@fairness.coop>
2023-05-02 16:04:30 +0200

    Display Symfony error responses instead of "Content missing"

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -9,3 +9,29 @@ export const app = startStimulusApp(require.context(
 
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+
+// Register Turbo event handlers
+// See: https://turbo.hotwired.dev/reference/events
+
+document.addEventListener('turbo:frame-missing', (event) => {
+    _displayAnyDebugExceptionPage(event);
+});
+
+const _displayAnyDebugExceptionPage = (event) => {
+    /** @type {Response} */
+    const response = event.detail.response;
+
+    if (response.status !== 500 || !response.headers.has('X-Debug-Exception')) {
+        // Most likely not a Symfony debug error response.
+        return;
+    }
+
+    event.preventDefault();
+
+    response.text().then(html => {
+        document.open();
+        document.write(html);
+        document.close();
+        history.pushState({}, '', response.url);
+    });
+};
