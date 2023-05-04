d9099609250460cd5963e5a5b4af16a14df4623e
Florimond Manca <florimond.manca@fairness.coop>
2023-05-04 16:45:31 +0200

    Fix form error handling in AddRegulationController (#319)

--- src/Infrastructure/Controller/Regulation/AddRegulationController.php
+++ src/Infrastructure/Controller/Regulation/AddRegulationController.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Controller\Regulation;
 
+use App\Application\CommandBusInterface;
 use App\Application\Regulation\Command\SaveRegulationGeneralInfoCommand;
+use App\Domain\User\Exception\OrganizationAlreadyHasRegulationOrderWithThisIdentifierException;
 use App\Infrastructure\Form\Regulation\GeneralInfoFormType;
 use App\Infrastructure\Security\SymfonyUser;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class AddRegulationController
 {
@@ -19,7 +25,9 @@ final class AddRegulationController
         private \Twig\Environment $twig,
         private FormFactoryInterface $formFactory,
         private Security $security,
+        private TranslatorInterface $translator,
         private RouterInterface $router,
+        private CommandBusInterface $commandBus,
     ) {
     }
 
@@ -28,25 +36,62 @@ final class AddRegulationController
         name: 'app_regulation_add',
         methods: ['GET', 'POST'],
     )]
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
         /** @var SymfonyUser */
         $user = $this->security->getUser();
 
+        $command = new SaveRegulationGeneralInfoCommand();
+
         $form = $this->formFactory->create(
             type: GeneralInfoFormType::class,
-            data: new SaveRegulationGeneralInfoCommand(),
+            data: $command,
             options: [
                 'organizations' => [$user->getOrganization()],
-                'action' => $this->router->generate('fragment_regulations_general_info_form'),
+                'action' => $this->router->generate('app_regulation_add'),
+                'save_options' => [
+                    'label' => 'common.form.continue',
+                    'attr' => [
+                        'class' => 'fr-btn fr-btn--icon-right fr-icon-arrow-right-line',
+                    ],
+                ],
             ],
         );
+
+        $form->handleRequest($request);
+        $hasCommandFailed = false;
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $regulationOrderRecord = $this->commandBus->handle($command);
+
+                return new RedirectResponse(
+                    url: $this->router->generate('app_regulation_detail', [
+                        'uuid' => $regulationOrderRecord->getUuid(),
+                    ]),
+                    status: Response::HTTP_SEE_OTHER,
+                );
+            } catch (OrganizationAlreadyHasRegulationOrderWithThisIdentifierException) {
+                $hasCommandFailed = true;
+                $form->get('identifier')->addError(
+                    new FormError(
+                        $this->translator->trans('regulation.general_info.error.identifier', [], 'validators'),
+                    ),
+                );
+            }
+        }
 
         return new Response(
             $this->twig->render(
                 name: 'regulation/create.html.twig',
-                context: ['form' => $form->createView()],
+                context: [
+                    'form' => $form->createView(),
+                    'cancelUrl' => $this->router->generate('app_regulations_list'),
+                ],
             ),
+            status: ($form->isSubmitted() && !$form->isValid()) || $hasCommandFailed
+                ? Response::HTTP_UNPROCESSABLE_ENTITY
+                : Response::HTTP_OK,
         );
     }
 }
