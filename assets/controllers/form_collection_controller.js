abd7ab9580759399fe63c41f9890be1879087423
Florimond Manca <florimond.manca@fairness.coop>
2023-05-11 11:43:51 +0200

    Allow deleting measures (#327)

--- assets/controllers/form_collection_controller.js
+++ assets/controllers/form_collection_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['collectionContainer'];
+    static targets = ['collectionContainer', 'collectionItem'];
 
     static values = {
         index: Number,
@@ -13,5 +13,16 @@ export default class extends Controller {
         el.innerHTML = this.prototypeValue.replace(/__name__/g, this.indexValue).replace(/__oneBasedIndex__/g, this.indexValue + 1);
         this.collectionContainerTarget.appendChild(el.children[0]);
         this.indexValue++;
+    }
+
+    syncIndices(_event) {
+        this.indexValue = this.collectionItemTargets.length;
+
+        this.collectionItemTargets.forEach((el, index) => {
+            el.querySelectorAll('[data-form-collection-indexed-template]').forEach(templatedEl => {
+                const text = templatedEl.dataset.formCollectionIndexedTemplate.replace(/__\\?oneBasedIndex__/g, index + 1);
+                templatedEl.textContent = text;
+            })
+        });
     }
 }
