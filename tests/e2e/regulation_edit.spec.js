33f5451f491c8a760d2789e2ded5dba8a10db2a0
florimondmanca <florimond.manca@fairness.coop>
2023-06-21 13:50:16 +0200

    Refactor regulation edit E2E tests

--- tests/e2e/regulation_edit.spec.js
+++ tests/e2e/regulation_edit.spec.js
@@ -1,169 +1,120 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test: base, expect } = require('@playwright/test');
+const { RegulationOrderPage } = require('./pages/regulation_order.page');
+
+const test = base.extend({
+    regulationOrderPage: async ({ page }, use) => {
+        const regulationOrderPage = new RegulationOrderPage(page);
+        await use(regulationOrderPage);
+        await regulationOrderPage.reset();
+    }
+});
 
 test.use({ storageState: 'playwright/.auth/mathieu.json' });
 
-test('Manage regulation location measures', async ({ page }) => {
-    /**
-     * Add first location and a maesure
-     */
+test('Begin then cancel a location', async ({ regulationOrderPage }) => {
+    /** @type {RegulationOrderPage} */
+    let page = regulationOrderPage;
 
-    await page.goto('/regulations/b1a3e982-39a1-4f0e-8a6f-ea2fd5e872c2');
-
-    // Check regulation has no location yet
-    const locations = page.getByRole('region', { name: 'Localisations' }).getByRole('list').first();
-    expect(await locations.locator('> li').count()).toBe(0);
-
-    // Fill mandatory location fields
-    await page.locator('text=Voie ou ville').fill('Route du Grand Brossais, 44260 Savenay');
-
-    // Fill empty measure
-    const measureItem1 = page.getByTestId('measure-list').getByRole('listitem').nth(0);
-    expect(await measureItem1.getByRole('heading', { level: 4 }).innerText()).toBe('Restriction 1');
-    await measureItem1.getByRole('combobox', { name: 'Type de restriction' }).selectOption({ label: 'Circulation à sens unique' });
-    await measureItem1.getByRole('radiogroup', { name: 'Véhicules concernés' }).getByText('Tous les véhicules', { exact: true }).click();
-    // Restricted vehicles options are not shown
-    await expect(measureItem1.getByLabel('Poids lourds', { exact: true })).not.toBeVisible();
-    // Exempted vehicles options are not shown
-    await expect(measureItem1.getByLabel('Bus', { exact: true })).not.toBeVisible();
-
-    // Submit and check location was added with the given measure
-    await page.getByRole('button', { name: 'Valider' }).click();
-    const locationItem = locations.locator('> li').first();
-
-    await locationItem.getByRole('heading', { level: 3, name: 'Route du Grand Brossais' }).waitFor();
-
-    expect(locationItem).toContainText('Circulation à sens unique');
-
-    /**
-     * Add another measure to the now-existing location
-     */
-
-    // Enter location edit mode
-    await locationItem.getByRole('button', { name: 'Modifier' }).click();
-    const measureList = locationItem.getByRole('list', { name: 'Liste des restrictions' });
-    await measureList.waitFor();
-    expect(await measureList.locator('> li').count()).toBe(1);
-
-    // Add a new measure
-    await locationItem.getByRole('button', { name: 'Ajouter une restriction' }).click();
-    const measureItem2 = measureList.locator('> li').nth(1);
-    expect(await measureItem2.getByRole('heading', { level: 4 }).innerText()).toBe('Restriction 2');
-    const restrictionType2 = measureItem2.getByRole('combobox', { name: 'Type de restriction' });
-    expect(await restrictionType2.getAttribute('name')).toBe('location_form[measures][1][type]'); // Check field index.
-    await restrictionType2.selectOption({ label: 'Circulation interdite' });
-
-    // Define restricted vehicles
-    const restrictedVehicles2 = measureItem2.getByRole('radiogroup', { name: 'Véhicules concernés' });
-    await restrictedVehicles2.getByText('Certains véhicules', { exact: true }).click();
-    await restrictedVehicles2.getByText('Poids lourds', { exact: true }).click();
-    const otherRestrictedVehicleTypeText2 = restrictedVehicles2.getByRole('textbox', { name: 'Autres' });
-    await expect(otherRestrictedVehicleTypeText2).not.toBeVisible();
-    await restrictedVehicles2.getByLabel('Autres', { exact: true }).click();
-    await otherRestrictedVehicleTypeText2.fill('Matières dangereuses');
-    // Define exempted vehicles
-    const exemptedVehicles2 = measureItem2.getByRole('group', { name: 'Sauf...'});
-    await exemptedVehicles2.getByRole('button', { name: 'Définir une exception' }).click();
-    await exemptedVehicles2.getByText('Bus', { exact: true }).click();
-    const otherExemptedVehicleTypeText2 = exemptedVehicles2.getByRole('textbox', { name: 'Autres' });
-    await expect(otherExemptedVehicleTypeText2).not.toBeVisible();
-    await exemptedVehicles2.getByLabel('Autres', { exact: true }).click();
-    await otherExemptedVehicleTypeText2.fill('Déchets industriels');
-
-    // Add a period
-    await measureItem2.getByRole('button', { name: 'Ajouter un créneau horaire' }).click();
-    const periodItem = measureItem2.getByTestId('period-list').getByRole('listitem').nth(0);
-    await periodItem.getByText('Lundi').click();
-    await periodItem.getByText('Mardi').click();
-    await periodItem.getByLabel("Heure de début").fill('08:00');
-    await periodItem.getByLabel("Heure de fin").fill('08:30');
-
-    // Submit and check new measure is shown
-    await locationItem.getByRole('button', { name: 'Valider' }).click();
-
-    await expect(locationItem).toContainText('Circulation à sens unique');
-    await expect(locationItem).toContainText('Circulation interdite du lundi au mardi de 08h00 à 08h30');
-
-    // Check restricted vehicles and exempted vehicles were saved
-    await locationItem.getByRole('button', { name: 'Modifier' }).click();
-    expect(await restrictedVehicles2.getByText('Poids lourds', { exact: true }).getAttribute('aria-pressed')).toBe('true');
-    expect(await restrictedVehicles2.getByLabel('Autres', { exact: true }).getAttribute('aria-pressed')).toBe('true');
-    await expect(restrictedVehicles2.getByRole('textbox', { name: 'Autres' })).toHaveValue('Matières dangereuses');
-    expect(await exemptedVehicles2.getByText('Bus', { exact: true }).getAttribute('aria-pressed')).toBe('true');
-    expect(await exemptedVehicles2.getByLabel('Autres', { exact: true }).getAttribute('aria-pressed')).toBe('true');
-    await expect(exemptedVehicles2.getByRole('textbox', { name: 'Autres' })).toHaveValue('Déchets industriels');
-    await locationItem.getByRole('button', { name: 'Annuler' }).click();
-
-    /**
-     * Change an existing measure
-     */
-
-    // Enter location edit mode
-    await locationItem.getByRole('button', { name: 'Modifier' }).click();
-    await measureList.waitFor();
-    expect(await measureList.locator('> li').count()).toBe(2);
-
-    // Change existing measure
-    await measureItem2.getByRole('combobox', { name: 'Type de restriction' }).selectOption({ label: 'Circulation alternée' });
-    await measureItem2.getByRole('radiogroup', { name: 'Véhicules concernés' }).getByText('Tous les véhicules', { exact: true }).click();
-    await expect(restrictedVehicles2.getByText('Poids lourds', { exact: true })).not.toBeVisible();
-    await expect(exemptedVehicles2.getByText('Bus', { exact: true })).toBeVisible();
-
-    // Submit and check measure has been updated
-    await locationItem.getByRole('button', { name: 'Valider' }).click();
-
-    await expect(locationItem).toContainText('Circulation à sens unique');
-    await expect(locationItem).toContainText('Circulation alternée');
-
-    /**
-     * Delete a measure
-     */
-
-    // Enter location edit mode
-    await locationItem.getByRole('button', { name: 'Modifier' }).click();
-    await measureList.waitFor();
-
-    // Delete 2nd measure
-    await measureItem2.getByRole('button', { name: 'Supprimer', exact: true }).nth(0).click();
-    expect(await measureList.locator('> li').count()).toBe(1);
-
-    // Add a new 2nd measure
-    await locationItem.getByRole('button', { name: 'Ajouter une restriction' }).click();
-    expect(await measureItem2.getByRole('heading', { level: 4 }).innerText()).toBe('Restriction 2');
-    expect(await restrictionType2.getAttribute('name')).toBe('location_form[measures][2][type]'); // Check field index.
-    await restrictionType2.selectOption({ label: 'Limitation de vitesse' });
-    await measureItem2.getByRole('radiogroup', { name: 'Véhicules concernés' }).getByText('Tous les véhicules', { exact: true }).click();
-
-    // Submit and check old 2nd measure has disappeared, and new 2nd measure has appeared.
-    await locationItem.getByRole('button', { name: 'Valider' }).click();
-    await expect(locationItem).toContainText('Circulation à sens unique');
-    await expect(locationItem).toContainText('Limitation de vitesse tous les jours');
-
-    // Check new 2nd measure does not have any periods.
-    await locationItem.getByRole('button', { name: 'Modifier' }).click();
-    await measureList.waitFor();
-    expect(await measureItem2.getByRole('group', { name: 'Créneaux horaires' }).locator('> li').count()).toBe(0);
+    await page.gotoRegulationOrderWithLocations();
+    await page.beginNewLocation();
+    await page.cancelLocation();
 });
 
-test('Delete a period', async ({ page }) => {
-    await page.goto('/regulations/e413a47e-5928-4353-a8b2-8b7dda27f9a5');
+test('Add a minimal location', async ({ regulationOrderPage }) => {
+    /** @type {RegulationOrderPage} */
+    let page = regulationOrderPage;
 
-    // Open first location
-    const locations = page.getByRole('region', { name: 'Localisations' }).getByRole('list').first();
-    const locationItem = locations.locator('> li').first();
+    await page.gotoRegulationOrderWithLocations();
+    const location = await page.addLocation({ address: 'Rue Monge, 21000 Dijon', restrictionType: 'Circulation interdite', expectedTitle: 'Rue Monge' });
+    await expect(location).toContainText('Circulation interdite tous les jours');
+});
 
-    await expect(locationItem).toContainText('Circulation interdite du lundi au mardi de 08h00 à 22h00');
+test('Add a minimal measure to a location', async ({ regulationOrderPage }) => {
+    /** @type {RegulationOrderPage} */
+    let page = regulationOrderPage;
 
-    await locationItem.getByRole('button', { name: 'Modifier' }).click();
+    await page.gotoRegulationOrderWithLocations();
+    const location = await page.addLocation({ address: 'Rue Monge, 21000 Dijon', restrictionType: 'Circulation interdite', expectedTitle: 'Rue Monge' });
+    await page.addMinimalMeasureToLocation(location, {
+        expectedIndex: 1,
+        expectedPosition: 2,
+        restrictionType: 'Circulation à sens unique',
+    });
+    await expect(location).toContainText('Circulation interdite tous les jours');
+    await expect(location).toContainText('Circulation à sens unique tous les jours');
+});
 
-    let measureList = locationItem.getByRole('list', { name: 'Liste des restrictions' });
-    await measureList.waitFor();
+test('Remove a measure and add another one to location', async ({ regulationOrderPage }) => {
+    /** @type {RegulationOrderPage} */
+    let page = regulationOrderPage;
 
-    const measureItem = measureList.locator('> li').nth(0);
-    expect(await measureItem.getByRole('heading', { level: 4 }).innerText()).toBe('Restriction 1');
+    await page.gotoRegulationOrderWithLocations();
+    const location = await page.addLocation({ address: 'Rue Monge, 21000 Dijon', restrictionType: 'Circulation interdite', expectedTitle: 'Rue Monge' });
 
-    await measureItem.getByTestId('delete-period-1').click();
+    await page.addMinimalMeasureToLocation(location, {
+        expectedIndex: 1,
+        expectedPosition: 2,
+        restrictionType: 'Circulation à sens unique',
+    });
+    await expect(location).toContainText('Circulation interdite tous les jours');
+    await expect(location).toContainText('Circulation à sens unique tous les jours');
 
-    await page.getByRole('button', { name: 'Valider' }).click();
-    await expect(locationItem).toContainText('Circulation interdite tous les jours');
+    await page.removeMeasureAndAddAnotherOne(location, {
+        indexToRemove: 1,
+        expectedIndex: 2,
+        expectedPosition: 2,
+        restrictionType: 'Limitation de vitesse',
+    });
+    await expect(location).toContainText('Circulation interdite tous les jours');
+    await expect(location).not.toContainText('Circulation à sens unique tous les jours');
+    await expect(location).toContainText('Limitation de vitesse tous les jours');
+});
+
+test('Set vehicles on a measure', async ({ regulationOrderPage }) => {
+    /** @type {RegulationOrderPage} */
+    let page = regulationOrderPage;
+
+    await page.gotoRegulationOrderWithLocations();
+    const location = await page.addLocation({ address: 'Rue Monge, 21000 Dijon', restrictionType: 'Circulation interdite', expectedTitle: 'Rue Monge' });
+    await page.setVehiclesOnMeasureAndAssertChangesWereSaved(location, {
+        measureIndex: 0,
+        restrictedVehicleTypes: ['Poids lourds'],
+        otherRestrictedVehicleType: 'Matières dangereuses',
+        exemptedVehicleTypes: ['Bus'],
+        otherExemptedVehicleType: 'Déchets industriels',
+    });
+});
+
+test('Add a period to a measure', async ({ regulationOrderPage }) => {
+    /** @type {RegulationOrderPage} */
+    let page = regulationOrderPage;
+
+    await page.gotoRegulationOrderWithLocations();
+    const location = await page.addLocation({ address: 'Rue Monge, 21000 Dijon', restrictionType: 'Circulation interdite', expectedTitle: 'Rue Monge' });
+    await expect(location).toContainText('Circulation interdite tous les jours');
+    await page.addPeriodToMeasure(location, {
+        measureIndex: 0,
+        days: ['Lundi', 'Mardi'],
+        startTime: '08:00',
+        endTime: '08:30',
+    });
+    await expect(location).toContainText('Circulation interdite du lundi au mardi de 08h00 à 08h30');
+});
+
+test('Delete a period from a measure', async ({ regulationOrderPage }) => {
+    /** @type {RegulationOrderPage} */
+    let page = regulationOrderPage;
+
+    await page.gotoRegulationOrderWithLocations();
+    const location = await page.addLocation({ address: 'Rue Monge, 21000 Dijon', restrictionType: 'Circulation interdite', expectedTitle: 'Rue Monge' });
+    await page.addPeriodToMeasure(location, {
+        measureIndex: 0,
+        days: ['Lundi'],
+        startTime: '08:00',
+        endTime: '16:00',
+    });
+    await expect(location).toContainText('Circulation interdite le lundi de 08h00 à 16h00');
+    await page.removePeriodFromMeasure(location, { measureIndex: 0, periodIndex: 0 });
+    await expect(location).toContainText('Circulation interdite tous les jours');
 });
