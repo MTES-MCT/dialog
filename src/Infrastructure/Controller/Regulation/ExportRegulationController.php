39f29baceb88c81bb5cbc4d103d132a0da218b14
mmarchois <mathieu.marchois@gmail.com>
2023-07-18 13:56:02 +0200

    display export link when a location has been defined

--- src/Infrastructure/Controller/Regulation/ExportRegulationController.php
+++ src/Infrastructure/Controller/Regulation/ExportRegulationController.php
@@ -8,20 +8,17 @@ use App\Application\QueryBusInterface;
 use App\Application\Regulation\Query\GetRegulationOrderRecordSummaryQuery;
 use App\Domain\Regulation\Specification\CanOrganizationAccessToRegulation;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\HeaderUtils;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
-final class DownloadRegulationController extends AbstractRegulationController
+final class ExportRegulationController extends AbstractRegulationController
 {
     public function __construct(
         QueryBusInterface $queryBus,
         CanOrganizationAccessToRegulation $canOrganizationAccessToRegulation,
         Security $security,
-        private readonly string $generatedRegulationOrderFolder,
         private readonly \Twig\Environment $twig,
         private readonly string $projectDir,
     ) {
@@ -29,9 +26,10 @@ final class DownloadRegulationController extends AbstractRegulationController
     }
 
     #[Route(
-        '/regulations/{uuid}/download',
-        name: 'app_regulation_download',
-        requirements: ['uuid' => Requirement::UUID],
+        '/regulations/{uuid}/export.{_format}',
+        name: 'app_regulation_export',
+        defaults: ['_format' => 'docx'],
+        requirements: ['uuid' => Requirement::UUID, '_format' => 'docx'],
         methods: ['GET'],
     )]
     public function __invoke(string $uuid): Response
@@ -47,25 +45,18 @@ final class DownloadRegulationController extends AbstractRegulationController
             ],
         );
 
-        $file = sprintf('%s%s.docx', $this->generatedRegulationOrderFolder, $uuid);
-        $filesystem = new Filesystem();
-        $filesystem->touch($file);
-        $filesystem->appendToFile(
-            $file,
+        $response = new Response(
             (new \Pandoc\Pandoc())
-                ->from('markdown')
-                ->input($content)
-                ->option('reference-doc', $this->projectDir . '/data/regulation-order-template.docx')
-                ->to('docx')
-                ->run(),
+            ->from('markdown')
+            ->input($content)
+            ->option('reference-doc', $this->projectDir . '/data/regulation-order-template.docx')
+            ->option('resource-path', $this->projectDir . '/public/images')
+            ->to('docx')
+            ->run(),
         );
-
-        $response = new BinaryFileResponse($file);
+        $disposition = HeaderUtils::makeDisposition(HeaderUtils::DISPOSITION_ATTACHMENT, sprintf('%s.docx', $uuid));
         $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-        $response->setContentDisposition(
-            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            sprintf('%s.docx', $uuid),
-        );
+        $response->headers->set('Content-Disposition', $disposition);
 
         return $response;
     }
