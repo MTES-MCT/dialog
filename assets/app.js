f1438da1bea49a58d9ccf6bf21fddcb245aa4fdf
florimondmanca <florimond.manca@fairness.coop>
2023-07-19 14:52:10 +0200

    Attempt: defer CSS loading

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,18 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+/**
+ * Application main JS file.
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { registerTurboEventHandlers } from './lib';
 
-// start the Stimulus application
-import './bootstrap';
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+export const app = startStimulusApp(require.context(
+    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    true,
+    /\.[jt]sx?$/
+));
+
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
+
+registerTurboEventHandlers();
--- webpack.config.js
+++ webpack.config.js
@@ -23,12 +23,13 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('styles', './assets/styles/app.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
+    // .splitEntryChunks()
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
