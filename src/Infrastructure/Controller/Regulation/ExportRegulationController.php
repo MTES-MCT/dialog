111161f1f99ba14b7626c958759604c65f59e368
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2023-07-19 10:29:14 +0200

    Export regulation order record to docx (#402)
    * Export regulation order record to docx
    
    * Add reference doc
    
    * Continue reference doc
    
    * continue
    
    * Bring back the spaces
    
    * continue
    
    * continue
    
    * continue
    
    * move regulation order template
    
    * Add tests
    
    * change pandoc version
    
    * display export link when a location has been defined
    
    ---------
    
    Co-authored-by: florimondmanca <florimond.manca@fairness.coop>

---
+++ src/Infrastructure/Controller/Regulation/ExportRegulationController.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller\Regulation;
+
+use App\Application\QueryBusInterface;
+use App\Application\Regulation\Query\GetRegulationOrderRecordSummaryQuery;
+use App\Domain\Regulation\Specification\CanOrganizationAccessToRegulation;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+final class ExportRegulationController extends AbstractRegulationController
+{
+    public function __construct(
+        QueryBusInterface $queryBus,
+        CanOrganizationAccessToRegulation $canOrganizationAccessToRegulation,
+        Security $security,
+        private readonly \Twig\Environment $twig,
+        private readonly string $projectDir,
+    ) {
+        parent::__construct($queryBus, $security, $canOrganizationAccessToRegulation);
+    }
+
+    #[Route(
+        '/regulations/{uuid}/export.{_format}',
+        name: 'app_regulation_export',
+        defaults: ['_format' => 'docx'],
+        requirements: ['uuid' => Requirement::UUID, '_format' => 'docx'],
+        methods: ['GET'],
+    )]
+    public function __invoke(string $uuid): Response
+    {
+        $regulationOrderRecord = $this->getRegulationOrderRecordUsing(function () use ($uuid) {
+            return $this->queryBus->handle(new GetRegulationOrderRecordSummaryQuery($uuid));
+        });
+
+        $content = $this->twig->render(
+            name: 'regulation/export.md.twig',
+            context: [
+                'regulationOrderRecord' => $regulationOrderRecord,
+            ],
+        );
+
+        $response = new Response(
+            (new \Pandoc\Pandoc())
+            ->from('markdown')
+            ->input($content)
+            ->option('reference-doc', $this->projectDir . '/data/regulation-order-template.docx')
+            ->option('resource-path', $this->projectDir . '/public/images')
+            ->to('docx')
+            ->run(),
+        );
+        $disposition = HeaderUtils::makeDisposition(HeaderUtils::DISPOSITION_ATTACHMENT, sprintf('%s.docx', $uuid));
+        $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+        $response->headers->set('Content-Disposition', $disposition);
+
+        return $response;
+    }
+}
