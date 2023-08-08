768b4f314c16b97d6d77de3c07da2dc4f3ef5a80
Johan Richer <johan.richer@multi.coop>
2023-08-08 10:21:55 +0200

    Fix e2e tests for download page

--- tests/e2e/regulation_publish.spec.js
+++ tests/e2e/regulation_publish.spec.js
@@ -13,13 +13,13 @@ test('Create then publish a regulation order', async ({ page }) => {
     await page.getByRole('button', { name: 'Continuer' }).click();
 
     await page.getByRole('heading', { level: 2, name: 'Arrêté permanent F01/test' }).waitFor();
-    await expect(page.getByRole('link', { name: 'Export format DOC' })).not.toBeVisible();
+    await expect(page.getByRole('link', { name: 'Télécharger le document' })).not.toBeVisible();
     await expect(page.getByRole('button', { name: 'Publier' })).toBeDisabled();
 
     const regPage = new RegulationOrderPage(page);
     const location = await regPage.addLocation({ address: 'Rue Monge, 21000 Dijon', restrictionType: 'Circulation interdite', expectedTitle: 'Rue Monge' }, { doBegin: false });
     await expect(location).toContainText('Circulation interdite tous les jours');
-    await page.getByRole('link', { name: 'Export format DOC' }).waitFor();
+    await page.getByRole('link', { name: 'Télécharger le document' }).waitFor();
     await expect(page.getByRole('button', { name: 'Publier' })).toBeEnabled();
 
     // Clean up
