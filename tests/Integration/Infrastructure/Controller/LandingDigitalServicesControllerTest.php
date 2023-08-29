adce568df795cfc4ae8162c4562ad399aab46357
Lealefoulon <lea.lefoulon@gmail.com>
2023-08-29 14:41:07 +0200

    correctifs

--- tests/Integration/Infrastructure/Controller/LandingDigitalServicesControllerTest.php
+++ tests/Integration/Infrastructure/Controller/LandingDigitalServicesControllerTest.php
@@ -9,7 +9,7 @@ final class LandingDigitalServicesControllerTest extends AbstractWebTestCase
     public function testPage(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/digitalservices');
+        $crawler = $client->request('GET', '/services-numeriques');
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertSecurityHeaders();
@@ -21,11 +21,11 @@ final class LandingDigitalServicesControllerTest extends AbstractWebTestCase
             ['a', 'Accéder aux données', ['href' => 'https://www.data.gouv.fr/fr/datasets/64947a4af5faf2f1f9eee299/']],
             ['h2', 'Comment ça marche ?'],
             ['h2', 'Des données qualitatives'],
-            ['a', 'point d’accès national aux données de transport.', ['href' => 'https://www.enable-javascript.com/fr/']],
+            ['a', 'point d’accès national aux données de transport.', ['href' => 'https://transport.data.gouv.fr/']],
             ['a', 'DATEX II', ['href' => 'https://www.datex2.eu/']],
             ['a', 'Contactez-nous', ['href' => 'mailto:dialog@beta.gouv.fr']],
             ['h2', 'Pourquoi numériser la réglementation ?'],
-            ['a', 'Loi Climat et Résilience', ['href' => 'https://www.ecologie.gouv.fr/loi-climat-resilience']],
+            ['a', 'Loi Climat et Résilience', ['href' => 'https://www.vie-publique.fr/loi/278460-loi-22-aout-2021-climat-et-resilience-convention-citoyenne-climat']],
             ['a', 'Contactez-nous', ['href' => 'mailto:dialog@beta.gouv.fr']],
             ['h2', 'Un déploiement continu'],
             ['h3', 'Expérimentation'],
