b629192de1d677780de2d69c5fbf3b1947dc52f6
Lealefoulon <116151499+Lealefoulon@users.noreply.github.com>
2023-09-07 14:59:05 +0200

    Home page fixes (#471)
    * default grey & metatitle
    
    * modifications trad
    
    * remplacement de mention grey par default grey

--- tests/Integration/Infrastructure/Controller/LandingControllerTest.php
+++ tests/Integration/Infrastructure/Controller/LandingControllerTest.php
@@ -13,14 +13,14 @@ final class LandingControllerTest extends AbstractWebTestCase
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertSecurityHeaders();
-        $this->assertSame('Numériser la réglementation de circulation routière avec Dialog', $crawler->filter('h1')->text());
+        $this->assertSame('Numériser la réglementation de circulation routière avec DiaLog', $crawler->filter('h1')->text());
         $this->assertSame('/collectivites', $crawler->selectLink('Pour les collectivités')->attr('href'));
         $this->assertSame('/services-numeriques', $crawler->selectLink('Pour les services numériques')->attr('href'));
         $this->assertSame('/usagers', $crawler->selectLink('Pour les usagers de la route')->attr('href'));
         $joinLink = $crawler->selectLink("Participer à l'expérimentation");
         $this->assertSame('Participer à l\'expérimentation', $joinLink->text());
         $this->assertSame('/collectivites', $joinLink->attr('href'));
-        $this->assertMetaTitle('Numériser la réglementation de circulation routière avec Dialog - DiaLog', $crawler);
+        $this->assertMetaTitle('DiaLog', $crawler);
         $contactLink = $crawler->filter('[data-testid="contact-link"]');
         $this->assertSame('Nous contacter', $contactLink->text());
         $this->assertSame('mailto:dialog@beta.gouv.fr', $contactLink->attr('href'));
@@ -32,7 +32,7 @@ final class LandingControllerTest extends AbstractWebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseStatusCodeSame(200);
-        $this->assertSame('Numériser la réglementation de circulation routière avec Dialog', $crawler->filter('h1')->text());
+        $this->assertSame('Numériser la réglementation de circulation routière avec DiaLog', $crawler->filter('h1')->text());
         $userLinks = $crawler->filter('[data-testid="user-links"]')->filter('li');
         $this->assertCount(2, $userLinks);
         $this->assertSame('Mathieu MARCHOIS', $userLinks->eq(0)->text());
