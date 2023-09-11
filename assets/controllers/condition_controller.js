a81a59569632a256664c1243b0e017ffebb20e4a
florimondmanca <florimond.manca@fairness.coop>
2023-09-11 15:30:03 +0200

    Create controller for form reveal behavior

--- assets/controllers/condition_controller.js
+++ assets/controllers/condition_controller.js
@@ -3,17 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static values = {
         equals: String,
-        checked: String,
     };
-
-    static targets = ['checkbox'];
 
     dispatchFromInputChange(event) {
         this.dispatch(event.target.value === this.equalsValue ? 'yes' : 'no');
     }
 
-    dispatchFromCheckboxes() {
-        const checkedValues = this.checkboxTargets.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-        this.dispatch(checkedValues.includes(this.checkedValue) ? 'yes' : 'no');
+    dispatchFromCheckboxChange(event) {
+        this.dispatch(event.target.checked ? 'yes' : 'no');
     }
 }
