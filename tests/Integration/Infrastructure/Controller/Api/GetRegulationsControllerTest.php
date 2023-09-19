7a4de0c5ff388e165a8117fd817a684a973fef55
Florimond Manca <florimond.manca@fairness.coop>
2023-09-19 15:06:06 +0200

    Validate compliance to DATEX II XSD (#487)
    * Validate compliance to DATEX II XSD
    
    * Fix order of vehicle characteristic fields

--- tests/Integration/Infrastructure/Controller/Api/GetRegulationsControllerTest.php
+++ tests/Integration/Infrastructure/Controller/Api/GetRegulationsControllerTest.php
@@ -17,6 +17,11 @@ final class GetRegulationsControllerTest extends AbstractWebTestCase
         $this->assertSame('text/xml; charset=UTF-8', $response->headers->get('content-type'));
         $this->assertResponseStatusCodeSame(200);
         $this->assertSecurityHeaders();
+
+        $xml = new \DOMDocument();
+        $xml->loadXML($response->getContent(), \LIBXML_NOBLANKS);
+        $this->assertTrue($xml->schemaValidate(self::$kernel->getProjectDir() . '/docs/spec/datex2/DATEXII_3_D2Payload.xsd'));
+
         $this->assertXmlStringEqualsXmlFile(
             __DIR__ . '/get-regulations-expected-result.xml',
             $response->getContent(),
