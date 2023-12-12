6ca182f204fafb04074838f8afbf5573138afdfb
florimondmanca <florimond.manca@fairness.coop>
2023-12-12 16:54:19 +0100

    Finish migration

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20231206135155.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20231206135155.php
@@ -22,12 +22,24 @@ final class Version20231206135155 extends AbstractMigration
         $this->addSql('ALTER TABLE location ADD city_label VARCHAR(255)');
         $this->addSql('ALTER TABLE location ADD road_name VARCHAR(255)');
 
-        // TODO: how to get city_code? Call API Address? Prepare offline configurable mapping file?
-        $this->addSql("UPDATE location SET city_label = substring(address from '\d{5} (.+)\$');");
         $this->addSql("UPDATE location SET road_name = substring(address from '^([^\d,]+),? \d{5}');");
+        $this->addSql("UPDATE location SET city_label = substring(address from '\d{5} (.+)\$');");
+        $this->addSql("
+            UPDATE location SET city_code = (
+                CASE
+                    WHEN city_label = 'Paris' THEN '751' || substring(address from '750(\d{2}) .+\$')
+                    WHEN city_label = 'Marseille' THEN '132' || substring(address from '130(\d{2}) .+\$')
+                    WHEN city_label = 'Lyon' THEN '6938' || substring(address from '6900(\d) .+\$')
+                    ELSE (
+                        SELECT insee_code FROM fr_city AS c
+                        WHERE c.departement = substring(address from '(\d{2})\d{3} .+\$')
+                        AND lower(c.name) = lower(city_label)
+                    )
+                END
+            )
+        ");
 
-        // TODO
-        // $this->addSql('ALTER TABLE location ALTER COLUMN city_code SET NOT NULL');
+        $this->addSql('ALTER TABLE location ALTER COLUMN city_code SET NOT NULL');
         $this->addSql('ALTER TABLE location ALTER COLUMN city_label SET NOT NULL');
         $this->addSql('ALTER TABLE location ALTER COLUMN road_name SET NOT NULL');
     }
@@ -37,5 +49,6 @@ final class Version20231206135155 extends AbstractMigration
         $this->addSql('ALTER TABLE location DROP city_code');
         $this->addSql('ALTER TABLE location DROP city_label');
         $this->addSql('ALTER TABLE location DROP road_name');
+        $this->addSql('ALTER TABLE location ALTER COLUMN address SET NOT NULL');
     }
 }
