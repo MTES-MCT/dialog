525ff74bc2fdbaac8ed76604e8544ab0c98ecc5d
florimondmanca <florimond.manca@fairness.coop>
2024-01-16 12:01:22 +0100

    Finish password hash rollback

--- src/Infrastructure/Persistence/Doctrine/Fixtures/UserFixture.php
+++ src/Infrastructure/Persistence/Doctrine/Fixtures/UserFixture.php
@@ -11,27 +11,26 @@ use Doctrine\Persistence\ObjectManager;
 final class UserFixture extends Fixture
 {
     public const MAIN_ORG_USER_EMAIL = 'mathieu.marchois@beta.gouv.fr';
-    public const PASSWORD = 'password';
-    private const PASSWORD_HASH = '$2y$13$dW.50KfdEYLYcTm3/y.Im.Xpq0ifXCOsDxWAPo76i27Mpniaa02ti';
     public const MAIN_ORG_ADMIN_EMAIL = 'mathieu.fernandez@beta.gouv.fr';
     public const OTHER_ORG_USER_EMAIL = 'florimond.manca@beta.gouv.fr';
+    public const PASSWORD = 'password';
 
     public function load(ObjectManager $manager): void
     {
         $mainOrgUser = (new User('0b507871-8b5e-4575-b297-a630310fc06e'))
             ->setFullName('Mathieu MARCHOIS')
             ->setEmail(self::MAIN_ORG_USER_EMAIL)
-            ->setPassword(self::PASSWORD_HASH);
+            ->setPassword(self::PASSWORD);
 
         $mainOtherAdmin = (new User('5bc831a3-7a09-44e9-aefa-5ce3588dac33'))
             ->setFullName('Mathieu FERNANDEZ')
             ->setEmail(self::MAIN_ORG_ADMIN_EMAIL)
-            ->setPassword(self::PASSWORD_HASH);
+            ->setPassword(self::PASSWORD);
 
         $otherOrgUser = (new User('d47badd9-989e-472b-a80e-9df642e93880'))
             ->setFullName('Florimond MANCA')
             ->setEmail(self::OTHER_ORG_USER_EMAIL)
-            ->setPassword(self::PASSWORD_HASH);
+            ->setPassword(self::PASSWORD);
 
         $manager->persist($mainOrgUser);
         $manager->persist($mainOtherAdmin);
