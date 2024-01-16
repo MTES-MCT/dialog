2d2af237a040bfc473420d644044bcf258651341
Florimond Manca <florimond.manca@fairness.coop>
2024-01-16 11:48:38 +0100

    Mise à jour intégration Eudonet (#589)
    * Mise à jour intégration Eudonet
    
    * Update tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Mathieu MARCHOIS <mathieu.marchois@gmail.com>
    
    ---------
    
    Co-authored-by: Mathieu MARCHOIS <mathieu.marchois@gmail.com>

--- tests/Unit/Application/EudonetParis/Command/ImportEudonetParisRegulationCommandHandlerTest.php
+++ tests/Unit/Application/EudonetParis/Command/ImportEudonetParisRegulationCommandHandlerTest.php
@@ -8,6 +8,7 @@ use App\Application\CommandBusInterface;
 use App\Application\EudonetParis\Command\ImportEudonetParisRegulationCommand;
 use App\Application\EudonetParis\Command\ImportEudonetParisRegulationCommandHandler;
 use App\Application\EudonetParis\Exception\ImportEudonetParisRegulationFailedException;
+use App\Application\Exception\GeocodingFailureException;
 use App\Application\Regulation\Command\PublishRegulationCommand;
 use App\Application\Regulation\Command\SaveRegulationGeneralInfoCommand;
 use App\Application\Regulation\Command\SaveRegulationLocationCommand;
@@ -86,6 +87,31 @@ final class ImportEudonetParisRegulationCommandHandlerTest extends TestCase
 
         $handler = new ImportEudonetParisRegulationCommandHandler($this->commandBus);
         $command = new ImportEudonetParisRegulationCommand($generalInfoCommand, []);
+
+        $handler($command);
+    }
+
+    public function testErrorGeocodingFailure(): void
+    {
+        $this->expectException(ImportEudonetParisRegulationFailedException::class);
+
+        $generalInfoCommand = $this->createMock(SaveRegulationGeneralInfoCommand::class);
+        $locationCommand = $this->createMock(SaveRegulationLocationCommand::class);
+        $regulationOrderRecord = $this->createMock(RegulationOrderRecord::class);
+
+        $matcher = self::exactly(2);
+        $this->commandBus
+            ->expects($matcher)
+            ->method('handle')
+            ->willReturnCallback(
+                fn ($command) => match ($matcher->getInvocationCount()) {
+                    1 => $this->assertEquals($generalInfoCommand, $command) ?: $regulationOrderRecord,
+                    2 => $this->assertEquals($locationCommand, $command) ?: throw new GeocodingFailureException('Could not geocode'),
+                },
+            );
+
+        $handler = new ImportEudonetParisRegulationCommandHandler($this->commandBus);
+        $command = new ImportEudonetParisRegulationCommand($generalInfoCommand, [$locationCommand]);
 
         $handler($command);
     }
