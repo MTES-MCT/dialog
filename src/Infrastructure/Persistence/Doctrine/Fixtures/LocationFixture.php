b5ac8e38ddd708083c76a4a23b067e9d33f8aa47
Florimond Manca <florimond.manca@fairness.coop>
2024-02-06 10:54:34 +0100

    Add CIFS incident feed (#521)
    * POC: CIFS incident feed
    
    * Handle schedule
    
    * Advance
    
    * Use new period system
    
    * Address feedback
    
    * Use latest cifsv2.xsd
    
    * Update file names
    
    * Keep only measures without vehicle restrictions
    
    * Address new period-timeSlot association
    
    * Implement polyline from geometry
    
    * Update tests and fixtures
    
    * Fix tests
    
    * Address review

--- src/Infrastructure/Persistence/Doctrine/Fixtures/LocationFixture.php
+++ src/Infrastructure/Persistence/Doctrine/Fixtures/LocationFixture.php
@@ -161,6 +161,20 @@ final class LocationFixture extends Fixture implements DependentFixtureInterface
             ]),
         );
 
+        $cifsLocation = new Location(
+            '06548f85-d545-7b45-8000-8a23c45850b3',
+            $this->getReference('regulationOrderCifs'),
+            cityCode: '82121',
+            cityLabel: 'Montauban (82000)',
+            roadName: 'Rue de la République',
+            fromHouseNumber: '21',
+            toHouseNumber: '33',
+            geometry: GeoJSON::toLineString([
+                Coordinates::fromLonLat(1.35500, 44.01630),
+                Coordinates::fromLonLat(1.35419, 44.01665),
+            ]),
+        );
+
         $manager->persist($typicalRegulationOrderLocation1);
         $manager->persist($typicalRegulationOrderLocation2);
         $manager->persist($typicalRegulationOrderLocation3);
@@ -170,6 +184,7 @@ final class LocationFixture extends Fixture implements DependentFixtureInterface
         $manager->persist($fullCityLocation);
         $manager->persist($permanentRegulationOrderLocation);
         $manager->persist($locationNoMeasures);
+        $manager->persist($cifsLocation);
         $manager->flush();
 
         $this->addReference('typicalLocation', $typicalRegulationOrderLocation1);
@@ -179,6 +194,7 @@ final class LocationFixture extends Fixture implements DependentFixtureInterface
         $this->addReference('fullCityLocation', $fullCityLocation);
         $this->addReference('permanentRegulationOrderLocation', $permanentRegulationOrderLocation);
         $this->addReference('locationNoMeasures', $locationNoMeasures);
+        $this->addReference('cifsLocation', $cifsLocation);
     }
 
     public function getDependencies(): array
