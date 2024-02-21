9e455474fadb8cd7e85000b30c1ceb226bdb12cd
florimondmanca <florimond.manca@fairness.coop>
2024-02-21 18:23:35 +0100

    Fix "database not in sync" check error

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20240221171053.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20240221171053.php
@@ -17,10 +17,12 @@ final class Version20240221171053 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE location_new RENAME TO location');
+        $this->addSql('ALTER INDEX idx_a31cfd096a61612 RENAME TO IDX_5E9E89CB96A61612');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE location RENAME TO location_new');
+        $this->addSql('ALTER INDEX IDX_5E9E89CB96A61612 RENAME TO idx_a31cfd096a61612');
     }
 }
