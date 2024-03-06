fcf93d06ff8aa7e5f8a3de62fd1290eea1a05237
Florimond Fairness <florimond.manca@fairness.coop>
2024-03-06 14:20:33 +0100

    Move PostGisGeometryService

--- src/Infrastructure/Persistence/Geometry/PostGisGeometryService.php
+++ src/Infrastructure/Persistence/Geometry/PostGisGeometryService.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\Adapter;
+namespace App\Infrastructure\Persistence\Geometry;
 
 use App\Application\Exception\GeocodingFailureException;
 use App\Application\GeometryServiceInterface;
