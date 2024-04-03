98a7e2810b1c7f746479739cb0f92d8e03b94524
Julien Jacquelinet <julien.jacquelinet@gmail.com>
2024-04-03 11:41:30 +0200

    renaming addSQL to addSql

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20240329102233.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20240329102233.php
@@ -24,7 +24,7 @@ final class Version20240329102233 extends AbstractMigration
                 (uuid, full_name, email, password) VALUES
                 (\'13ad7a59-bd3c-4e1d-8f85-8b148ad00705\', \'Julien JACQUELINET\', \'julien.jacquelinet@beta.gouv.fr\', \'$2y$13$0vx4Anj.TVy8D0iGrER4zuKT//vYlnEx1bLpoUZnv2lNPmWNWXrQC\')
         ');
-        $this->addSQL("
+        $this->addSql("
             INSERT INTO organizations_users (user_uuid, organization_uuid) VALUES
                 ('13ad7a59-bd3c-4e1d-8f85-8b148ad00705', 'e0d93630-acf7-4722-81e8-ff7d5fa64b66')
         ");
