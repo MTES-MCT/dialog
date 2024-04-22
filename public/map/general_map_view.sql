e01330012168a39dc7d7b08c80a249ee9056a4ee
Julien Jacquelinet <julien.jacquelinet@gmail.com>
2024-04-22 14:58:15 +0200

    when clicking on a single geometry on the map, we show the other geometries associated to the same regulation, if any

--- public/map/general_map_view.sql
+++ public/map/general_map_view.sql
@@ -2,9 +2,30 @@ CREATE OR REPLACE VIEW general_map_view AS
 SELECT location.road_name AS road_name, location.road_number AS road_number, measure.type AS "type",
        regulation_order.category AS category, regulation_order.description AS description, regulation_order.identifier AS identifier,
        (regulation_order.end_date IS NULL) AS is_permanent, (regulation_order_record.status = 'draft') AS is_draft,
-       organization.name AS organization_name, location.geometry AS geometry
+       organization.name AS organization_name, location.geometry AS geometry,
+       regulation_order.uuid AS regulation_order_id
 FROM location
 JOIN measure ON measure.uuid = location.measure_uuid
 JOIN regulation_order ON regulation_order.uuid = measure.regulation_order_uuid
 JOIN regulation_order_record ON regulation_order_record.regulation_order_uuid = regulation_order.uuid
 JOIN organization ON organization.uuid = regulation_order_record.organization_uuid;
+
+
+-- location.geometry may be LineString or MultiLineString -> before applying ST_Collect, we use ST_Dump to get on LineString per row, otherwise we may get an undisplayable GeometryCollection (ST_Collect of several MultiLineString returns a GeometryCollection)
+CREATE OR REPLACE VIEW general_map_view_aggregated AS
+WITH one_line_per_row AS (
+SELECT location.road_name, location.road_number, 
+       (ST_Dump(location.geometry)).geom AS geometry_as_line, 
+       regulation_order.uuid AS regulation_order_id
+FROM location
+JOIN measure ON measure.uuid = location.measure_uuid
+JOIN regulation_order ON regulation_order.uuid = measure.regulation_order_uuid
+JOIN regulation_order_record ON regulation_order_record.regulation_order_uuid = regulation_order.uuid
+JOIN organization ON organization.uuid = regulation_order_record.organization_uuid
+)
+SELECT regulation_order.uuid AS regulation_order_id,
+       array_agg(concat(E'\'', one_line_per_row.road_name, E'\'', '[', one_line_per_row.road_number, ']')) AS named_locations, 
+       ST_Multi(ST_Collect(one_line_per_row.geometry_as_line))::Geometry(MultiLineString,4326) AS geometry
+FROM one_line_per_row
+JOIN regulation_order ON regulation_order.uuid = one_line_per_row.regulation_order_id
+GROUP BY regulation_order.uuid;
--- public/map/map.js
+++ public/map/map.js
@@ -17,6 +17,13 @@ map.on('load', () => {
             url: 'http://localhost:3000/general_map_view'
 	}
     );
+    map.addSource(
+	'regulations-aggregated-source',
+	{
+            type: 'vector',
+            url: 'http://localhost:3000/general_map_view_aggregated'
+	}
+    );
     // layers (i.e. styles) :
     map.addLayer(
 	{
@@ -30,10 +37,29 @@ map.on('load', () => {
             },
             'paint': {
 		'line-color': '#ff69b4',
-		'line-width': 10
+		'line-width': 5
             }
 	},
 	"toponyme numéro de route - départementale" // insert this layer below the main label layers like road labels
+    );
+    // this layer 'regulations-aggregated-layer' is to display the scattered geometries of a regulation : when clicking on a single geometry, it simply shows the other geometries associated to the same regulation, if any
+    map.addLayer(
+	{
+            'id': 'regulations-aggregated-layer',
+            'type': 'line',
+            'source': 'regulations-aggregated-source',
+            'source-layer': 'general_map_view_aggregated',
+            'layout': {
+		'line-join': 'round',
+		'line-cap': 'round',
+		'visibility': 'none'  // hide this layer by default
+            },
+            'paint': {
+		'line-color': '#61B3F0',
+		'line-width': 10
+            }
+	},
+	"regulations-layer"
     );
     // popup when clicking on a feature of the regulation layer
     map.on('click', 'regulations-layer', (e) => {
@@ -41,17 +67,23 @@ map.on('load', () => {
             .setLngLat(e.lngLat)
             .setHTML((e.features[0].properties.road_name || "''") + " [" + (e.features[0].properties.road_number || "") + "]" + "<h3>" + e.features[0].properties.identifier + "</h3>" + e.features[0].properties.description + "<br />" + " • arrêté permanent = " + e.features[0].properties.is_permanent + "<br /> • arrêté à l'état de brouillon = " + e.features[0].properties.is_draft)
             .addTo(map);
-    });
+    });    
     // change the cursor when the mouse is over the regulations layer
-    map.on('mouseenter', 'regulations-layer', () => {
+    // also, highlight the geom(s) (one or many) associated to one regulation when the mouse is over one of its geometry
+    map.on('mouseenter', 'regulations-layer', (e) => {
         map.getCanvas().style.cursor = 'pointer';
+	let current_regulation_id = e.features[0].properties.regulation_order_id;
+	map.setFilter("regulations-aggregated-layer", ["==", "regulation_order_id", current_regulation_id]);
+	map.setLayoutProperty('regulations-aggregated-layer', 'visibility', 'visible');
     });
-    map.on('mouseleave', 'regulations-layer', () => {
+    map.on('mouseleave', 'regulations-layer', (e) => {
         map.getCanvas().style.cursor = '';
+	map.setFilter("regulations-aggregated-layer", null); // delete the filter
+	map.setLayoutProperty('regulations-aggregated-layer', 'visibility', 'none');
     });
     // filtering :
-    // this filter will display all regulations (means : (is_draft) OR (NOT is_draft) -> always TRUE)
-    map.setFilter("regulations-layer", ["any", ["==", "is_draft", true], ["==", "is_draft", false]]);
+    // demo : this filter will display all regulations (means : (is_draft) OR (NOT is_draft) -> always TRUE)
+    //map.setFilter("regulations-layer", ["any", ["==", "is_draft", true], ["==", "is_draft", false]]);    
 });
 
 map.on('idle', () => {
