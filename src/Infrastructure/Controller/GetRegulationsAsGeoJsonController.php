ed6afe4ace2b82f22e2775d48ab50aaffa16fb85
Julien Jacquelinet <julien.jacquelinet@gmail.com>
2024-05-06 11:15:23 +0200

    update the map without reloading the webpage : <turbo-frame> is working

--- src/Infrastructure/Controller/GetRegulationsAsGeoJsonController.php
+++ src/Infrastructure/Controller/GetRegulationsAsGeoJsonController.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Infrastructure\Controller;
 
 use App\Domain\Regulation\Repository\RegulationOrderRecordRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class GetRegulationsAsGeoJsonController
+final class GetRegulationsAsGeoJsonController extends AbstractController
 {
     public function __construct(
         private \Twig\Environment $twig,
@@ -33,6 +34,9 @@ final class GetRegulationsAsGeoJsonController
             $this->twig->render(
                 name: '_regulations_as_geojson.html.twig',
                 context: [
+                    'permanentAndOrTemporaryFilter' => $permanentAndOrTemporaryFilter,
+                    'draftFilter' => $draftFilter,
+
                     'regulationOrdersAsGeoJson' => $regulationOrdersAsGeoJson,
                 ],
             ),
