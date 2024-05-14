13adc9cf3aa2c9f77364946caaef1cb04e99bdee
florimondmanca <florimond.manca@fairness.coop>
2024-05-14 17:38:59 +0200

    Sections : applique une tolérance pour le calcul des morceaux de rattachement

--- src/Infrastructure/Adapter/LineSectionMaker.php
+++ src/Infrastructure/Adapter/LineSectionMaker.php
@@ -23,6 +23,7 @@ final class LineSectionMaker implements LineSectionMakerInterface
         string $lineGeometry,
         Coordinates $fromCoords,
         Coordinates $toCoords,
+        int|float $tolerance = 1, // Meters
     ): string {
         $includeCrs = str_contains($lineGeometry, '"crs"');
 
@@ -30,32 +31,44 @@ final class LineSectionMaker implements LineSectionMakerInterface
         $pointB = $toCoords->asGeoJSON($includeCrs);
 
         $row = $this->em->getConnection()->fetchAssociative(
-            'WITH linestring AS (
+            'WITH a AS (
+                SELECT ST_SetSRID(ST_GeomFromGeoJSON(:point_a), 4326) AS geom
+            ),
+            b AS (
+                SELECT ST_SetSRID(ST_GeomFromGeoJSON(:point_b), 4326) AS geom
+            ),
+            linestring AS (
                 -- Split the line geometry into its individual LINESTRING components
                 SELECT (components.dump).geom AS geom FROM (
-                    SELECT ST_Dump(ST_LineMerge(:geom)) AS dump
+                    SELECT ST_Dump(ST_LineMerge(ST_SetSRID(ST_GeomFromGeoJSON(:geom), 4326))) AS dump
                 ) AS components
             ),
-            -- For each point, find the LINESTRING(s) it is closest to (multiple closest line strings is a mostly theoretical case)
+            -- For each point, find the LINESTRING(s) it is closest to, within a tolerance radius
             linestring_a AS (
                 SELECT l.geom
                 FROM linestring AS l
-                WHERE ST_Distance(:point_a, l.geom) = (SELECT MIN(ST_Distance(:point_a, geom)) FROM linestring)
+                JOIN a ON true
+                -- ::geography ensures distances are computed in meters
+                WHERE ST_Distance(a.geom::geography, l.geom::geography) <= (SELECT MIN(ST_Distance(a.geom::geography, geom::geography)) FROM linestring) + :tolerance
             ),
             linestring_b AS (
                 SELECT l.geom
                 FROM linestring AS l
-                WHERE ST_Distance(:point_b, l.geom) = (SELECT MIN(ST_Distance(:point_b, geom)) FROM linestring)
+                JOIN b ON true
+                -- ::geography ensures distances are computed in meters
+                WHERE ST_Distance(b.geom::geography, l.geom::geography) <= (SELECT MIN(ST_Distance(b.geom::geography, geom::geography)) FROM linestring) + :tolerance
             )
             -- Compute the section on the LINESTRING to which both points belong to.
             SELECT ST_AsGeoJSON(
                 ST_LineSubstring(
                     l.geom,
-                    LEAST(ST_LineLocatePoint(l.geom, :point_a), ST_LineLocatePoint(l.geom, :point_b)),
-                    GREATEST(ST_LineLocatePoint(l.geom, :point_a), ST_LineLocatePoint(l.geom, :point_b))
+                    LEAST(ST_LineLocatePoint(l.geom, a.geom), ST_LineLocatePoint(l.geom, b.geom)),
+                    GREATEST(ST_LineLocatePoint(l.geom, a.geom), ST_LineLocatePoint(l.geom, b.geom))
                 )
             ) AS section
             FROM linestring_a AS l
+            JOIN a ON true
+            JOIN b ON true
             -- If the points belong to different LINESTRINGs, this join will be empty.
             INNER JOIN linestring_b ON l.geom = linestring_b.geom
             LIMIT 1
@@ -64,6 +77,7 @@ final class LineSectionMaker implements LineSectionMakerInterface
                 'geom' => $lineGeometry,
                 'point_a' => $pointA,
                 'point_b' => $pointB,
+                'tolerance' => $tolerance,
             ],
         );
 
