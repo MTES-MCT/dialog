1ca21db58af8470b7c7b1bc63c2370d675dad81f
florimondmanca <florimond.manca@fairness.coop>
2024-05-15 14:40:59 +0200

    Add example, add -o flag

--- tools/polyline_to_geojson.py
+++ tools/polyline_to_geojson.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import argparse
 import json
+import webbrowser
+from urllib.parse import quote
 
 
-def main(polylines: list[str]):
+def main(polylines: list[str], open_browser: bool = False):
     for polyline in polylines:
         # "40 2 41 2.5 ..." -> [40, 2, 41, 2.5]
         coords = [float(c) for c in polyline.split(" ")]
@@ -13,19 +15,23 @@ def main(polylines: list[str]):
 
         points = list(zip(xs, ys))  # -> [[2, 40], [2.5, 41]]
 
-        print(
-            json.dumps(
-                {
-                    "type": "LineString",
-                    "coordinates": points,
-                }
-            )
-        )
+        geometry = {"type": "LineString", "coordinates": points}
+        geojson = json.dumps(geometry)
+        print(geojson)
+
+        if open_browser:
+            # https://geojson.io/#url-api
+            url = "https://geojson.io/#data=data:application/json," + quote(geojson)
+            print(f"---> Opening: {url}")
+            webbrowser.open(url)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert CIFS polylines to GeoJSON")
     parser.add_argument("polyline", nargs="+")
+    parser.add_argument(
+        "-o", "--open-browser", action="store_true", help="Show result in geojson.io"
+    )
     args = parser.parse_args()
 
-    main(polylines=args.polyline)
+    main(polylines=args.polyline, open_browser=args.open_browser)
