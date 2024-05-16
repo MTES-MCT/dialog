2956b91216becf3c45b63be86a68263ef289cabb
Florimond Manca <florimond.manca@fairness.coop>
2024-05-16 10:16:04 +0200

    Sections : applique une tolérance pour le calcul des morceaux de rattachement (#771)

--- tests/Integration/Infrastructure/Adapter/LineSectionMakerTest.php
+++ tests/Integration/Infrastructure/Adapter/LineSectionMakerTest.php
@@ -7,11 +7,14 @@ namespace App\Tests\Integration\Infrastructure\Adapter;
 use App\Application\Exception\GeocodingFailureException;
 use App\Domain\Geography\Coordinates;
 use App\Infrastructure\Adapter\LineSectionMaker;
+use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 final class LineSectionMakerTest extends KernelTestCase
 {
+    /** @var Connection */
+    private $bdtopoConnection;
     private $lineGeometry;
     private $em;
     private $lineSectionMaker;
@@ -20,17 +23,20 @@ final class LineSectionMakerTest extends KernelTestCase
     {
         $container = self::getContainer();
 
+        $this->bdtopoConnection = $container->get('doctrine.dbal.bdtopo_connection');
+
         $lineStrings = [
             /*
-             * The coordinates below represent the 3 line strings "a", "b" and "c".
-             * They all intersect at the point marked as "+" (belongs to all line strings).
+             * The coordinates below represent the line strings "a", "b", "c" and "d".
+             * Line strings "a", "b" and "c" all intersect at the point marked as "+" (belongs to all line strings).
+             * There is a gap between "c" and "d".
              *
              * y (lat)
              * ^
-             * 3....b...
-             * 2aaa.+..c
-             * 1a......c
-             * 012345678> x (lon)
+             * 3....b.......
+             * 2aaa-+-c....d
+             * 1a.....c.....
+             * 0123456789012> x (lon)
              */
             [
                 // a
@@ -48,8 +54,13 @@ final class LineSectionMakerTest extends KernelTestCase
             [
                 // c
                 [5, 2],
-                [8, 2],
-                [8, 1],
+                [7, 2],
+                [7, 1],
+            ],
+            [
+                // d
+                [12, 2],
+                [13, 2],
             ],
         ];
 
@@ -62,6 +73,30 @@ final class LineSectionMakerTest extends KernelTestCase
 
         $this->em = $container->get(EntityManagerInterface::class);
         $this->lineSectionMaker = new LineSectionMaker($this->em);
+    }
+
+    /**
+     * Return the tolerance radius to apply so that $pointA and $pointB are exactly equally distant to $point.
+     */
+    private static function getThresholdTolerance(Connection $bdtopoConnection, Coordinates $point, Coordinates $pointA, Coordinates $pointB): float
+    {
+        $distA = $bdtopoConnection->fetchOne(
+            'SELECT ST_Distance(ST_GeomFromGeoJSON(:p)::geography, ST_GeomFromGeoJSON(:a)::geography)',
+            [
+                'p' => $point->asGeoJSON(),
+                'a' => $pointA->asGeoJSON(),
+            ],
+        );
+
+        $distB = $bdtopoConnection->fetchOne(
+            'SELECT ST_Distance(ST_GeomFromGeoJSON(:p)::geography, ST_GeomFromGeoJSON(:b)::geography)',
+            [
+                'p' => $point->asGeoJSON(),
+                'b' => $pointB->asGeoJSON(),
+            ],
+        );
+
+        return abs($distB - $distA);
     }
 
     private function provideComputeSection(): array
@@ -81,13 +116,13 @@ final class LineSectionMakerTest extends KernelTestCase
                 ]),
             ],
             'pointsNearSameLine' => [
-                'fromCoords' => Coordinates::fromLonLat(6, 1), // Maps to (6, 2) on b
-                'toCoords' => Coordinates::fromLonLat(9, 2), // Maps to (8, 2) on b
+                'fromCoords' => Coordinates::fromLonLat(6, 1), // Maps to (6, 2) and (7, 1) on c
+                'toCoords' => Coordinates::fromLonLat(9, 2), // Maps to (7, 2) on c
                 'section' => json_encode([
                     'type' => 'LineString',
                     'coordinates' => [
                         [6, 2],
-                        [8, 2],
+                        [7, 2],
                     ],
                 ]),
             ],
@@ -111,9 +146,27 @@ final class LineSectionMakerTest extends KernelTestCase
                     'type' => 'LineString',
                     'coordinates' => [
                         [5, 2],
-                        [8, 2],
+                        [7, 2],
                     ],
                 ]),
+            ],
+            'pointNearSameLineWithinTolerance' => [
+                'fromCoords' => Coordinates::fromLonLat(7, 1),
+                'toCoords' => Coordinates::fromLonLat(9.6, 2), // P
+                'section' => json_encode([
+                    'type' => 'LineString',
+                    'coordinates' => [
+                        [7, 2],
+                        [7, 1],
+                    ],
+                ]),
+                'getTolerance' => fn ($bdtopoConnection) => 1.01 // Just above
+                    * self::getThresholdTolerance(
+                        $bdtopoConnection,
+                        point: Coordinates::fromLonLat(9.6, 2), // P
+                        pointA: Coordinates::fromLonLat(7, 2), // Point of c that is closest to P
+                        pointB: Coordinates::fromLonLat(12, 2), // Point of d that is closest to P
+                    ),
             ],
         ];
     }
@@ -121,17 +174,30 @@ final class LineSectionMakerTest extends KernelTestCase
     /**
      * @dataProvider provideComputeSection
      */
-    public function testComputeSection(Coordinates $fromCoords, Coordinates $toCoords, string $section): void
+    public function testComputeSection(Coordinates $fromCoords, Coordinates $toCoords, string $section, ?\Closure $getTolerance = null): void
     {
-        $this->assertSame($section, $this->lineSectionMaker->computeSection($this->lineGeometry, $fromCoords, $toCoords));
+        $tolerance = $getTolerance ? $getTolerance($this->bdtopoConnection) : 1;
+
+        $this->assertSame($section, $this->lineSectionMaker->computeSection($this->lineGeometry, $fromCoords, $toCoords, $tolerance));
     }
 
     private function provideComputeSectionError(): array
     {
         return [
-            'pointsDoNotBelongToSameSegment' => [
-                'fromCoords' => Coordinates::fromLonLat(1, 1),
-                'toCoords' => Coordinates::fromLonLat(7, 3),
+            'pointsDoNotMapToSameSegment' => [
+                'fromCoords' => Coordinates::fromLonLat(1, 1), // Belongs to a
+                'toCoords' => Coordinates::fromLonLat(7, 3), // Maps to c
+            ],
+            'pointsMapOutsideTolerance' => [
+                'fromCoords' => Coordinates::fromLonLat(7, 1),
+                'toCoords' => Coordinates::fromLonLat(9.6, 2), // P
+                'getTolerance' => fn ($bdtopoConnection) => 0.99 // Just below
+                    * self::getThresholdTolerance(
+                        $bdtopoConnection,
+                        point: Coordinates::fromLonLat(9.6, 2), // P
+                        pointA: Coordinates::fromLonLat(7, 2), // Point of c that is closest to P
+                        pointB: Coordinates::fromLonLat(12, 2), // Point of d that is closest to P
+                    ),
             ],
         ];
     }
@@ -139,10 +205,11 @@ final class LineSectionMakerTest extends KernelTestCase
     /**
      * @dataProvider provideComputeSectionError
      */
-    public function testComputeSectionError(Coordinates $fromCoords, Coordinates $toCoords): void
+    public function testComputeSectionError(Coordinates $fromCoords, Coordinates $toCoords, ?\Closure $getTolerance = null): void
     {
-        $this->expectException(GeocodingFailureException::class);
+        $tolerance = $getTolerance ? $getTolerance($this->bdtopoConnection) : 1;
 
-        $this->lineSectionMaker->computeSection($this->lineGeometry, $fromCoords, $toCoords);
+        $this->expectException(GeocodingFailureException::class);
+        $this->lineSectionMaker->computeSection($this->lineGeometry, $fromCoords, $toCoords, $tolerance);
     }
 }
