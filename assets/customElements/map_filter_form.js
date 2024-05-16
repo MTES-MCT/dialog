933bf1cf20c7efa19daf7a739af6295b5a4bf783
Julien Jacquelinet <julien.jacquelinet@gmail.com>
2024-05-16 15:44:59 +0200

    save filtering map form parameters inside the url without reloading the webpage

--- assets/customElements/map_filter_form.js
+++ assets/customElements/map_filter_form.js
@@ -14,6 +14,22 @@ export default class extends HTMLElement {
 	    for (const formControl of form.elements) {
 		formControl.addEventListener('change', () => form.requestSubmit());
 	    }
+
+	    // update the url without reloading the page - complementary with 'data-turbo-action="replace"' inside the <form>
+	    // credits : https://www.30secondsofcode.org/js/s/modify-url-without-reload/
+	    const associatedTurboFrameId = this.dataset['turbo-frame-id'];
+	    const updateURLWith = this.dataset['update-url-with'];
+	    if (associatedTurboFrameId && updateURLWith) {
+		const associatedTurboFrame = document.getElementById(associatedTurboFrameId);
+		if (associatedTurboFrame) {
+		    associatedTurboFrame.addEventListener('turbo:frame-load', () => {
+			const queryString = window.location.search;
+			const currentTitle = document.title;
+			const nextState = {};
+			window.history.replaceState(nextState, currentTitle, `${updateURLWith}${queryString}`);
+		    });
+		}
+	    }
 	});
     }
 }
