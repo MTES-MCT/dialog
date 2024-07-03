2e5df966dd865b65a27f514e6439a22c8785d3d8
florimondmanca <florimond.manca@fairness.coop>
2024-07-03 11:38:14 +0200

    Refactor using fetchAllAssociative

--- src/Infrastructure/Persistence/Doctrine/Repository/Regulation/LocationRepository.php
+++ src/Infrastructure/Persistence/Doctrine/Repository/Regulation/LocationRepository.php
@@ -50,55 +50,69 @@ final class LocationRepository extends ServiceEntityRepository implements Locati
         bool $includeUpcomingRegulations = false,
         bool $includePastRegulations = false,
     ): string {
-        $query = $this->createQueryBuilder('l')
-               ->select('l.geometry, m.type, l.uuid')
-               ->innerJoin('l.measure', 'm')
-               ->innerJoin('m.regulationOrder', 'ro')
-               ->innerJoin('ro.regulationOrderRecord', 'roc')
-               ->where('roc.status = :status')
-               ->andWhere('l.geometry IS NOT NULL')
-               ->setParameter('status', RegulationOrderRecordStatusEnum::PUBLISHED);
+        $includeNone = !$includePermanentRegulations && !$includeTemporaryRegulations;
+        $permanentOnly = $includePermanentRegulations && !$includeTemporaryRegulations;
+        $temporaryOnly = !$includePermanentRegulations && $includeTemporaryRegulations;
 
-        if ($includePermanentRegulations && !$includeTemporaryRegulations) {
-            $query->andWhere('ro.endDate IS NULL');
-        } elseif (!$includePermanentRegulations && $includeTemporaryRegulations) {
-            $query->andWhere('ro.endDate IS NOT NULL');
-        } elseif (!$includePermanentRegulations && !$includeTemporaryRegulations) {
+        if ($includeNone) {
             return json_encode([
                 'type' => 'FeatureCollection',
                 'features' => [],
             ]); // we return no regulations
         }
-        // no filter needed for ($includePermanentRegulations && $includeTemporaryRegulations) : we want all kind of regulations
 
-        if (!$includeUpcomingRegulations && !$includePastRegulations) {
-            $query->andWhere('(ro.endDate >= :now OR ro.endDate IS NULL) AND ro.startDate <= :now');
-        } elseif ($includeUpcomingRegulations && !$includePastRegulations) {
-            $query->andWhere('((ro.endDate >= :now OR ro.endDate IS NULL) AND ro.startDate <= :now) OR ro.startDate > :now');
-        } elseif (!$includeUpcomingRegulations && $includePastRegulations) {
-            $query->andWhere('((ro.endDate >= :now OR ro.endDate IS NULL) AND ro.startDate <= :now) OR ro.endDate < :now');
-        }
-        // no filter needed for ($includeUpcomingRegulations && $includePastRegulations) : we want all kind of regulations
-        if (!($includeUpcomingRegulations && $includePastRegulations)) {
-            $query->setParameter('now', $this->dateUtils->getNow()->format('Y-m-d'));
-        }
+        $regulationTypeWhereClause =
+            $permanentOnly ? 'AND ro.end_date IS NULL'
+            : ($temporaryOnly ? 'AND ro.end_date IS NOT NULL'
+                : '');
 
-        $geoJSONs = [];
-        $results = $query->getQuery()->getResult();
-        foreach ($results as $result) {
-            $geoJSONs[] = [
+        $upcomingOnly = $includeUpcomingRegulations && !$includePastRegulations;
+        $pastOnly = !$includeUpcomingRegulations && $includePastRegulations;
+        $presentOnly = !$includeUpcomingRegulations && !$includePastRegulations;
+
+        $regulationDatesWhereClause =
+            $upcomingOnly ? 'AND ((ro.end_date >= :now OR ro.end_date IS NULL) AND ro.start_date <= :now) OR ro.start_date > :now'
+            : ($pastOnly ? 'AND ((ro.end_date >= :now OR ro.end_date IS NULL) AND ro.start_date <= :now) OR ro.end_date < :now'
+                : ($presentOnly ? 'AND (ro.end_date >= :now OR ro.end_date IS NULL) AND ro.start_date <= :now'
+                    : ''));
+
+        $rows = $this->getEntityManager()->getConnection()->fetchAllAssociative(
+            sprintf(
+                'SELECT ST_AsGeoJSON(l.geometry) AS geometry, m.type AS measure_type, l.uuid AS location_uuid
+                FROM location AS l
+                INNER JOIN measure AS m ON m.uuid = l.measure_uuid
+                INNER JOIN regulation_order AS ro ON ro.uuid = m.regulation_order_uuid
+                INNER JOIN regulation_order_record AS roc ON ro.uuid = roc.regulation_order_uuid
+                WHERE roc.status = :status
+                AND l.geometry IS NOT NULL
+                %s
+                %s
+                ',
+                $regulationTypeWhereClause,
+                $regulationDatesWhereClause,
+            ),
+            [
+                'status' => RegulationOrderRecordStatusEnum::PUBLISHED,
+                'now' => $this->dateUtils->getNow()->format('Y-m-d'),
+            ],
+        );
+
+        $features = [];
+
+        foreach ($rows as $row) {
+            $features[] = [
                 'type' => 'Feature',
-                'geometry' => json_decode($result['geometry']),
+                'geometry' => json_decode($row['geometry']),
                 'properties' => [
-                    'location_uuid' => $result['uuid'],
-                    'measure_type' => $result['type'],
+                    'location_uuid' => $row['location_uuid'],
+                    'measure_type' => $row['measure_type'],
                 ],
             ];
         }
 
         return json_encode([
             'type' => 'FeatureCollection',
-            'features' => $geoJSONs,
+            'features' => $features,
         ]);
     }
 }
