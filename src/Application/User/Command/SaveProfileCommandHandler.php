f29a9a1f43250df53ef14ca7d66ccc582df5d6f1
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2024-09-10 16:51:29 +0200

    Edit profile (#934)
    * Edit profile
    
    * update profile
    
    * tests unit
    
    * test integration
    
    * validation test
    
    * design & corrections
    
    * corrections
    
    * add testWithBadEmail
    
    ---------
    
    Co-authored-by: Lealefoulon <lea.lefoulon@gmail.com>
    Co-authored-by: Lealefoulon <116151499+Lealefoulon@users.noreply.github.com>

---
+++ src/Application/User/Command/SaveProfileCommandHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\User\Command;
+
+use App\Application\StringUtilsInterface;
+use App\Domain\User\Exception\EmailAlreadyExistsException;
+use App\Domain\User\Specification\IsEmailAlreadyExists;
+
+final class SaveProfileCommandHandler
+{
+    public function __construct(
+        private StringUtilsInterface $stringUtils,
+        private IsEmailAlreadyExists $isEmailAlreadyExists,
+    ) {
+    }
+
+    public function __invoke(SaveProfileCommand $command): void
+    {
+        $email = $this->stringUtils->normalizeEmail($command->email);
+        $user = $command->user;
+
+        if ($email !== $user->getEmail() && true === $this->isEmailAlreadyExists->isSatisfiedBy($email)) {
+            throw new EmailAlreadyExistsException();
+        }
+
+        $user->setEmail($email);
+        $user->setFullName($command->fullName);
+    }
+}
