3ca2e7f3ac604b9e84d524d0c7649910eb356bd8
Lealefoulon <lea.lefoulon@gmail.com>
2024-09-16 15:34:35 +0200

    unit test

---
+++ tests/Unit/Application/User/Command/SavePasswordCommandHandlerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Application\User\Command;
+
+use App\Application\PasswordHasherInterface;
+use App\Application\User\Command\SavePasswordCommand;
+use App\Application\User\Command\SavePasswordCommandHandler;
+use App\Domain\User\User;
+use PHPUnit\Framework\TestCase;
+
+final class SavePasswordCommandHandlerTest extends TestCase
+{
+    public function testUpdatePassword()
+    {
+        $user = $this->createMock(User::class);
+        $passwordHasher = $this->createMock(PasswordHasherInterface::class);
+
+        $passwordHasher
+            ->expects(self::once())
+            ->method('hash')
+            ->with('password')
+            ->willReturn('passwordHashed');
+
+        $user
+            ->expects(self::once())
+            ->method('setPassword')
+            ->with('passwordHashed');
+
+        $handler = new SavePasswordCommandHandler(
+            $passwordHasher,
+        );
+        $command = new SavePasswordCommand($user);
+        $command->password = 'password';
+
+        $handler($command);
+    }
+}
