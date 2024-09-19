d37d9078628bde2162fa08f3664f8ec87278af74
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2024-09-19 14:34:38 +0200

    Add admin link (#965)

--- tests/Integration/Infrastructure/Controller/MyArea/IndexControllerTest.php
+++ tests/Integration/Infrastructure/Controller/MyArea/IndexControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Infrastructure\Controller\MyArea;
 
+use App\Infrastructure\Persistence\Doctrine\Fixtures\UserFixture;
 use App\Tests\Integration\Infrastructure\Controller\AbstractWebTestCase;
 
 final class IndexControllerTest extends AbstractWebTestCase
@@ -21,6 +22,16 @@ final class IndexControllerTest extends AbstractWebTestCase
         $organizations = $crawler->filter('[data-testid="organization-list"]');
         $this->assertCount(1, $organizations->filter('[data-testid="organization-detail"]'));
         $this->assertSame('Main Org Contributeur', $organizations->filter('[data-testid="organization-detail"]')->text());
+        $this->assertCount(0, $crawler->filter('[data-testid="admin-link"]'));
+    }
+
+    public function testIndexAsAdmin(): void
+    {
+        $client = $this->login(UserFixture::MAIN_ORG_ADMIN_EMAIL);
+        $crawler = $client->request('GET', '/mon-espace');
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertCount(1, $crawler->filter('[data-testid="admin-link"]'));
     }
 
     public function testWithoutAuthenticatedUser(): void
