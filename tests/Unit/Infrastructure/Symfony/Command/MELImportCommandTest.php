573dcaf3c15d75a5bde13887b0e3dc6789113c60
Florimond Manca <florimond.manca@fairness.coop>
2024-09-24 13:21:58 +0200

    Extrait le code de reporting de l'intégration Litteralis (#957)
    * Extrait le Reporter de l'intégration Litteralis
    
    * Rename

--- tests/Unit/Infrastructure/Symfony/Command/MELImportCommandTest.php
+++ tests/Unit/Infrastructure/Symfony/Command/MELImportCommandTest.php
@@ -4,23 +4,44 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Infrastructure\Symfony\Command;
 
+use App\Application\DateUtilsInterface;
+use App\Infrastructure\IntegrationReport\Reporter;
 use App\Infrastructure\MEL\MELExecutor;
 use App\Infrastructure\Symfony\Command\MELImportCommand;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class MELImportCommandTest extends TestCase
 {
-    public function testExecute()
+    private $logger;
+    private $executor;
+    private $dateUtils;
+
+    protected function setUp(): void
     {
-        $executor = $this->createMock(MELExecutor::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->executor = $this->createMock(MELExecutor::class);
+        $this->dateUtils = $this->createMock(DateUtilsInterface::class);
+    }
 
-        $executor
+    public function testExecute(): void
+    {
+        $now = new \DateTimeImmutable('now');
+        $reporter = new Reporter($this->logger);
+
+        $this->dateUtils
             ->expects(self::once())
-            ->method('execute');
+            ->method('getNow')
+            ->willReturn($now);
 
-        $command = new MELImportCommand($executor);
+        $this->executor
+            ->expects(self::once())
+            ->method('execute')
+            ->with($now, self::equalTo($reporter));
+
+        $command = new MELImportCommand($this->logger, $this->executor, $this->dateUtils);
         $this->assertSame('app:mel:import', $command->getName());
 
         $commandTester = new CommandTester($command);
@@ -28,16 +49,14 @@ class MELImportCommandTest extends TestCase
         $commandTester->assertCommandIsSuccessful();
     }
 
-    public function testExecuteError()
+    public function testExecuteError(): void
     {
-        $executor = $this->createMock(MELExecutor::class);
-
-        $executor
+        $this->executor
             ->expects(self::once())
             ->method('execute')
             ->willThrowException(new \RuntimeException('Failed'));
 
-        $command = new MELImportCommand($executor);
+        $command = new MELImportCommand($this->logger, $this->executor, $this->dateUtils);
         $commandTester = new CommandTester($command);
         $commandTester->execute([]);
 
