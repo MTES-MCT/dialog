1795d0500558ea05dbe721125fe759dea0df8e00
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2024-09-24 17:34:22 +0200

    Duplicate visa model (#969)

---
+++ src/Application/VisaModel/Command/DuplicateVisaModelCommandHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\VisaModel\Command;
+
+use App\Application\CommandBusInterface;
+use App\Domain\VisaModel\Exception\VisaModelNotFoundException;
+use App\Domain\VisaModel\Repository\VisaModelRepositoryInterface;
+use App\Domain\VisaModel\VisaModel;
+
+final class DuplicateVisaModelCommandHandler
+{
+    public function __construct(
+        private VisaModelRepositoryInterface $visaModelRepository,
+        private CommandBusInterface $commandBus,
+    ) {
+    }
+
+    public function __invoke(DuplicateVisaModelCommand $command): void
+    {
+        $orginalVisaModel = $this->visaModelRepository->findOneByUuid($command->uuid);
+        if (!$orginalVisaModel instanceof VisaModel) {
+            throw new VisaModelNotFoundException();
+        }
+
+        $visaModelCommand = new SaveVisaModelCommand($orginalVisaModel->getOrganization());
+        $visaModelCommand->name = \sprintf('%s (copie)', $orginalVisaModel->getName());
+        $visaModelCommand->description = $orginalVisaModel->getDescription();
+        $visaModelCommand->visas = $orginalVisaModel->getVisas();
+
+        $this->commandBus->handle($visaModelCommand);
+    }
+}
