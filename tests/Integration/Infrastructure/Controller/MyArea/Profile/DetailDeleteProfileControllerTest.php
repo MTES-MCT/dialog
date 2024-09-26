f8e14404fb1705e3e9da841ee4448434bad47bb1
Lealefoulon <lea.lefoulon@gmail.com>
2024-09-26 12:18:17 +0200

    test integration

---
+++ tests/Integration/Infrastructure/Controller/MyArea/Profile/DetailDeleteProfileControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\Infrastructure\Controller\MyArea\Profile;
+
+use App\Tests\Integration\Infrastructure\Controller\AbstractWebTestCase;
+
+final class DetailDeleteProfileControllerTest extends AbstractWebTestCase
+{
+    public function testDetailDeleteProfilePage(): void
+    {
+        $client = $this->login('mathieu.fernandez@beta.gouv.fr');
+        $crawler = $client->request('GET', '/mon-espace/profile/delete/detail');
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertMetaTitle('Mathieu FERNANDEZ - DiaLog', $crawler);
+        $this->assertSame('Suppression du compte', $crawler->filter('h2')->text());
+    }
+}
