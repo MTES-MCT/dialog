4c61f35bec96d1d72775c079029974476b8fe958
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2024-10-29 14:27:35 +0100

    Ajout de la page vie privée (#1035)

---
+++ src/Infrastructure/Controller/PrivacyController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class PrivacyController
+{
+    public function __construct(
+        private \Twig\Environment $twig,
+    ) {
+    }
+
+    #[Route('/vie-privee', name: 'app_privacy', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        $response = (new Response($this->twig->render('privacy.html.twig')))->setSharedMaxAge(86400);
+        $response->headers->set(AbstractSessionListener::NO_AUTO_CACHE_CONTROL_HEADER, 'true');
+
+        return $response;
+    }
+}
