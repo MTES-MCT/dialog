d28d3f2ccb0a321c395511983431f6186a99e57d
Florimond Manca <florimond.manca@fairness.coop>
2024-10-30 11:56:08 +0100

    Intègre le dashboard Metabase à la page stats (#1037)
    * Intègre le dashboard Metabase à la page stats
    
    * Adjust height

--- tests/Integration/Infrastructure/Controller/StatisticsControllerTest.php
+++ tests/Integration/Infrastructure/Controller/StatisticsControllerTest.php
@@ -16,14 +16,15 @@ final class StatisticsControllerTest extends AbstractWebTestCase
         $this->assertMetaTitle('Statistiques - DiaLog', $crawler);
         $this->assertSame('Statistiques', $crawler->filter('h1')->text());
 
-        $stats = $crawler->filter('div.fr-card');
-        $this->assertSame(6, $stats->count());
+        $stats = $crawler->filter('[data-testid=stat]');
+        $this->assertSame(7, $stats->count());
 
-        $this->assertSame("Nombre total d'utilisateurs 3", $stats->eq(0)->text());
-        $this->assertSame("Nombre total d'organisations 2", $stats->eq(1)->text());
-        $this->assertSame("Nombre total d'arrếtés 1", $stats->eq(2)->text());
-        $this->assertSame("Nombre total d'arrếtés publiés 1", $stats->eq(3)->text());
-        $this->assertSame("Nombre total d'arrếtés permanents 0", $stats->eq(4)->text());
-        $this->assertSame("Nombre total d'arrếtés temporaires 1", $stats->eq(5)->text());
+        $this->assertSame('Tableau de bord des indicateurs', $stats->eq(0)->attr('title'));
+        $this->assertSame("Nombre total d'utilisateurs 3", $stats->eq(1)->text());
+        $this->assertSame("Nombre total d'organisations 2", $stats->eq(2)->text());
+        $this->assertSame("Nombre total d'arrếtés 1", $stats->eq(3)->text());
+        $this->assertSame("Nombre total d'arrếtés publiés 1", $stats->eq(4)->text());
+        $this->assertSame("Nombre total d'arrếtés permanents 0", $stats->eq(5)->text());
+        $this->assertSame("Nombre total d'arrếtés temporaires 1", $stats->eq(6)->text());
     }
 }
