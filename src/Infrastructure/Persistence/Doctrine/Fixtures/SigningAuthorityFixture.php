fdd3316ff4e53299b2c4c98ebb0400470749b396
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2024-11-12 16:51:29 +0100

    Ajout de la page detail de l'autorité signataire (#1067)

---
+++ src/Infrastructure/Persistence/Doctrine/Fixtures/SigningAuthorityFixture.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Doctrine\Fixtures;
+
+use App\Domain\Organization\SigningAuthority\SigningAuthority;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+final class SigningAuthorityFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $signatoryAuthority = new SigningAuthority(
+            uuid: '9cebe00d-04d8-48da-89b1-059f6b7bfe44',
+            name: 'Monsieur le maire de Savenay',
+            address: '3 rue de la Concertation',
+            placeOfSignature: 'Savenay',
+            signatoryName: 'Monsieur X, Maire de Savenay',
+            organization: $this->getReference('mainOrg'),
+        );
+
+        $manager->persist($signatoryAuthority);
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            OrganizationFixture::class,
+        ];
+    }
+}
