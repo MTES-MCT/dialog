aefb241ea401e9f5fe5a1ed07524673b0b7c556d
Lealefoulon <lea.lefoulon@gmail.com>
2024-11-13 18:58:01 +0100

    test integration

---
+++ tests/Integration/Infrastructure/Controller/Regulation/Fragments/DuplicateMeasureFragmentControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Infrastructure\Controller\Regulation\Fragments;
+
+use App\Infrastructure\Persistence\Doctrine\Fixtures\MeasureFixture;
+use App\Infrastructure\Persistence\Doctrine\Fixtures\RegulationOrderRecordFixture;
+use App\Tests\Integration\Infrastructure\Controller\AbstractWebTestCase;
+use App\Tests\SessionHelper;
+
+final class DuplicateMeasureFragmentControllerTest extends AbstractWebTestCase
+{
+    use SessionHelper;
+
+    public function testDuplicate(): void
+    {
+        $client = $this->login();
+
+        $crawler = $client->request('POST', '/_fragment/regulations/' . RegulationOrderRecordFixture::UUID_TYPICAL . '/measure/' . MeasureFixture::UUID_TYPICAL . '/duplicate', [
+            '_token' => $this->generateCsrfToken($client, 'duplicate-measure'),
+        ]);
+
+        $this->assertResponseStatusCodeSame(200);
+        $streams = $crawler->filter('turbo-stream');
+
+        $this->assertSame($streams->eq(0)->attr('action'), 'append');
+        $this->assertSame($streams->eq(1)->attr('target'), 'block_measure_' . MeasureFixture::UUID_TYPICAL);
+    }
+
+    public function testWithoutAuthenticatedUser(): void
+    {
+        $client = static::createClient();
+        $client->request('POST', '/_fragment/regulations/' . RegulationOrderRecordFixture::UUID_TYPICAL . '/measure/' . MeasureFixture::UUID_TYPICAL . '/duplicate');
+
+        $this->assertResponseRedirects('http://localhost/login', 302);
+    }
+}
