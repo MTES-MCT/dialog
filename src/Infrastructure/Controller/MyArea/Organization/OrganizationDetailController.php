c831a9ed8fd3519c031055a7cae0e4bfc7fe6d96
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2024-11-21 14:32:00 +0100

    Display organization logo (#1078)
    * Display organization logo
    
    * Fix permission setting
    
    ---------
    
    Co-authored-by: florimondmanca <florimond.manca@fairness.coop>

--- src/Infrastructure/Controller/MyArea/Organization/OrganizationDetailController.php
+++ src/Infrastructure/Controller/MyArea/Organization/OrganizationDetailController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\Controller\MyArea\Organization;
 
 use App\Application\QueryBusInterface;
+use App\Application\StorageInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +15,7 @@ final class OrganizationDetailController extends AbstractOrganizationController
 {
     public function __construct(
         private \Twig\Environment $twig,
+        private StorageInterface $storage,
         QueryBusInterface $queryBus,
         Security $security,
     ) {
@@ -29,12 +31,14 @@ final class OrganizationDetailController extends AbstractOrganizationController
     public function __invoke(string $uuid): Response
     {
         $organization = $this->getOrganization($uuid);
+        $logo = $organization->getLogo() ? $this->storage->get($organization->getLogo()) : null;
 
         return new Response(
             content: $this->twig->render(
                 name: 'my_area/organization/detail.html.twig',
                 context: [
                     'organization' => $organization,
+                    'logo' => $logo,
                 ],
             ),
         );
