69dbbc572ca4c32772a881c0e2e0183fc51c779f
Lealefoulon <lea.lefoulon@gmail.com>
2024-11-21 10:45:57 +0100

    road-maintenance-or-construction Datex

--- src/Domain/Regulation/Enum/VehicleTypeEnum.php
+++ src/Domain/Regulation/Enum/VehicleTypeEnum.php
@@ -19,7 +19,7 @@ enum VehicleTypeEnum: string
     case TAXI = 'taxi';
     case CAR_SHARING = 'carSharing';
     case HAZARDOUS_MATERIALS = 'hazardousMaterials';
-    case CITY_LOGISTICS = 'cityLogistics';
+    case ROAD_MAINTENANCE_OR_CONSTRUCTION = 'roadMaintenanceOrConstruction';
 
     public static function restrictedCases(): array
     {
@@ -41,7 +41,7 @@ enum VehicleTypeEnum: string
             VehicleTypeEnum::PEDESTRIANS,
             VehicleTypeEnum::TAXI,
             VehicleTypeEnum::CAR_SHARING,
-            VehicleTypeEnum::CITY_LOGISTICS,
+            VehicleTypeEnum::ROAD_MAINTENANCE_OR_CONSTRUCTION,
             VehicleTypeEnum::OTHER,
         ];
     }
