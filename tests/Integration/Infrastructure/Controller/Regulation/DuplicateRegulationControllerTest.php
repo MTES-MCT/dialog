3c1ae1fe5eb9a4a73d7732fdc053cfcb031ecfd5
Lealefoulon <116151499+Lealefoulon@users.noreply.github.com>
2024-12-10 16:27:42 +0100

    Update tests/Integration/Infrastructure/Controller/Regulation/DuplicateRegulationControllerTest.php
    Co-authored-by: Mathieu MARCHOIS <mathieu.marchois@gmail.com>

--- tests/Integration/Infrastructure/Controller/Regulation/DuplicateRegulationControllerTest.php
+++ tests/Integration/Infrastructure/Controller/Regulation/DuplicateRegulationControllerTest.php
@@ -28,7 +28,7 @@ final class DuplicateRegulationControllerTest extends AbstractWebTestCase
         // Measure
         $this->assertSame('Circulation interdite', $measures->eq(0)->filter('h3')->text());
         $this->assertSame('pour tous les véhicules', $measures->eq(0)->filter('.app-card__content li')->eq(0)->text());
-        $this->assertSame('à partir du 11/03/2023 à 00h00', $measures->eq(0)->filter('.app-card__content li')->eq(1)->text()); // Date comes from DateUtilsMock
+        $this->assertSame('à partir du 11/03/2023 à 00h00', $measures->eq(0)->filter('.app-card__content li')->eq(1)->text());
         $this->assertSame('Rue du Simplon à Paris 18e Arrondissement (75018)', $measures->eq(0)->filter('.app-card__content li')->eq(3)->text());
     }
 
