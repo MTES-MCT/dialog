4ada0930590d5134f0849ed131f06940f5cc54fe
florimondmanca <florimond.manca@fairness.coop>
2025-01-07 15:27:42 +0100

    Ajuste la page stats pour le nouveau dashboard Metabase

--- src/Infrastructure/Controller/StatisticsController.php
+++ src/Infrastructure/Controller/StatisticsController.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Controller;
 
-use App\Application\QueryBusInterface;
-use App\Application\Regulation\Query\GetStatisticsQuery;
 use App\Infrastructure\Adapter\MetabaseEmbedFactory;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +12,6 @@ final class StatisticsController
 {
     public function __construct(
         private \Twig\Environment $twig,
-        private QueryBusInterface $queryBus,
         private MetabaseEmbedFactory $metabaseEmbedFactory,
     ) {
     }
@@ -22,12 +19,9 @@ final class StatisticsController
     #[Route('/stats', name: 'app_stats', methods: ['GET'])]
     public function __invoke(): Response
     {
-        $statistics = $this->queryBus->handle(new GetStatisticsQuery());
-
         $dashboardEmbedUrl = $this->metabaseEmbedFactory->makeDashboardUrl();
 
         return new Response($this->twig->render('statistics.html.twig', [
-            'statistics' => $statistics,
             'dashboardEmbedUrl' => $dashboardEmbedUrl,
         ]));
     }
