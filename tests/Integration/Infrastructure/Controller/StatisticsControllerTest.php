3342a6b48cd91d2ab2734b71420bfcb394b7bbfa
Florimond Manca <florimond.manca@fairness.coop>
2025-01-08 10:29:06 +0100

    Ajuste la page stats pour le nouveau dashboard Metabase (#1135)

--- tests/Integration/Infrastructure/Controller/StatisticsControllerTest.php
+++ tests/Integration/Infrastructure/Controller/StatisticsControllerTest.php
@@ -16,15 +16,7 @@ final class StatisticsControllerTest extends AbstractWebTestCase
         $this->assertMetaTitle('Statistiques - DiaLog', $crawler);
         $this->assertSame('Statistiques', $crawler->filter('h1')->text());
 
-        $stats = $crawler->filter('[data-testid=stat]');
-        $this->assertSame(7, $stats->count());
-
-        $this->assertSame('Tableau de bord des indicateurs', $stats->eq(0)->attr('title'));
-        $this->assertSame("Nombre total d'utilisateurs 3", $stats->eq(1)->text());
-        $this->assertSame("Nombre total d'organisations 2", $stats->eq(2)->text());
-        $this->assertSame("Nombre total d'arrếtés 1", $stats->eq(3)->text());
-        $this->assertSame("Nombre total d'arrếtés publiés 1", $stats->eq(4)->text());
-        $this->assertSame("Nombre total d'arrếtés permanents 0", $stats->eq(5)->text());
-        $this->assertSame("Nombre total d'arrếtés temporaires 1", $stats->eq(6)->text());
+        $dashboard = $crawler->filter('[data-testid=dashboard]')->eq(0);
+        $this->assertSame('Tableau de bord des indicateurs', $dashboard->attr('title'));
     }
 }
