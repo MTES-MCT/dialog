a4183abcd35f280e9a40239e7750e2a2fa2718f1
florimondmanca <florimond.manca@fairness.coop>
2025-01-14 16:12:07 +0100

    Rassemble les intégrations Litteralis en une seule

--- tests/Integration/Infrastructure/Symfony/Command/LitteralisImportCommandTest.php
+++ tests/Integration/Infrastructure/Symfony/Command/LitteralisImportCommandTest.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Infrastructure\Symfony\Command;
 
-use App\Infrastructure\Symfony\Command\FougeresImportCommand;
+use App\Infrastructure\Symfony\Command\LitteralisImportCommand;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
-final class FougeresImportCommandTest extends KernelTestCase
+final class LitteralisImportCommandTest extends KernelTestCase
 {
     public function testExecute(): void
     {
         self::bootKernel();
         $container = static::getContainer();
 
-        $command = $container->get(FougeresImportCommand::class);
+        $command = $container->get(LitteralisImportCommand::class);
         $commandTester = new CommandTester($command);
 
         $commandTester->execute([]);
