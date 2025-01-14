bbf6f0fb6b31e919d377994f5e8e1b1411d530c1
Florimond Manca <florimond.manca@fairness.coop>
2025-01-14 15:02:29 +0100

    Désactive la navigation JS (Turbo Drive) par défaut (#1144)

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -11,4 +11,9 @@ export const app = startStimulusApp(require.context(
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
 
+// Désactive Turbo Drive par défaut, pour des raisons d'accessibilité
+// On peut toujours réactiver Turbo Drive au cas par cas avec data-turbo="true"
+// https://turbo.hotwired.dev/reference/drive#turbo.session.drive
+Turbo.session.drive = false;
+
 registerTurboEventHandlers();
