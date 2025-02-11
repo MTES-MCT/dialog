6ecbd030c8384f86a1ba76f746cb25dec385c729
florimondmanca <florimond.manca@fairness.coop>
2025-02-11 14:57:28 +0100

    POC : Content tracking avec Matomo

--- assets/matomo.js
+++ assets/matomo.js
@@ -4,6 +4,15 @@ _paq.push(["setDoNotTrack", true]);
 _paq.push(["disableCookies"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+
+// Active le Content Tracking
+// On utilise trackAllContentImpressions plutôt que trackVisibleContentImpressions
+// car ce dernier déclenche un scan périodique de tout le DOM, ce qui
+// est coûteux pour le terminal.
+// Le tracking des interactions permet tout de même d'étudier le ratio interactions / impressions.
+// https://developer.matomo.org/guides/content-tracking
+_paq.push(['trackAllContentImpressions']);
+
 (function () {
     var u = "https://stats.beta.gouv.fr/";
     _paq.push(['setTrackerUrl', u + 'matomo.php']);
