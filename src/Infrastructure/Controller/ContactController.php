ef2f8d40f5203394fb7c40bad0dd2a2fb45dc53b
Lealefoulon <lea.lefoulon@gmail.com>
2025-02-19 19:10:17 +0100

    new contact page

---
+++ src/Infrastructure/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class ContactController
+{
+    public function __construct(
+        private \Twig\Environment $twig,
+    ) {
+    }
+
+    #[Route('/contact', name: 'app_contact', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        return new Response($this->twig->render('contact.html.twig'));
+    }
+}
