cbb9fb1c77c3b9e6a59a07180a77038c47527bdc
Lealefoulon <lea.lefoulon@gmail.com>
2025-02-20 16:41:39 +0100

    landing details

---
+++ src/Infrastructure/Controller/LandingDetailsController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class LandingDetailsController
+{
+    public function __construct(
+        private \Twig\Environment $twig,
+    ) {
+    }
+
+    #[Route('/details', name: 'app_landing_details', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        return new Response($this->twig->render('landing_details.html.twig'));
+    }
+}
