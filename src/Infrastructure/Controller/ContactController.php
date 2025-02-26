c935d9c764bb82fb80db371ae3279caefe353135
Florimond Manca <florimond.manca@fairness.coop>
2025-02-26 10:25:47 +0100

    Refonte des landings (reprise) (#1217)
    * Refonte de la page d'accueil
    
    * Ajoute la page Saisir ou intégrer des arrêtés
    
    ---------
    
    Co-authored-by: Lealefoulon <lea.lefoulon@gmail.com>

--- src/Infrastructure/Controller/ContactController.php
+++ src/Infrastructure/Controller/ContactController.php
@@ -7,16 +7,16 @@ namespace App\Infrastructure\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class LandingDigitalServicesController
+final class ContactController
 {
     public function __construct(
         private \Twig\Environment $twig,
     ) {
     }
 
-    #[Route('/services-numeriques', name: 'app_landing_digital_services', methods: ['GET'])]
+    #[Route('/contact', name: 'app_contact', methods: ['GET'])]
     public function __invoke(): Response
     {
-        return new Response($this->twig->render('landing_digital_services.html.twig'));
+        return new Response($this->twig->render('contact.html.twig'));
     }
 }
---
+++ tests/Integration/Infrastructure/Controller/LandingDetailsControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Infrastructure\Controller;
+
+final class LandingDetailsControllerTest extends AbstractWebTestCase
+{
+    public function testLanding(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/details');
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertSecurityHeaders();
+
+        $this->assertMetaTitle('Saisir ou intégrer des arrêtés - DiaLog', $crawler);
+
+        $this->assertSkipLinks(
+            [
+                ['Contenu', '#content'],
+                ['Menu', '#header-navigation'],
+                ['Pied de page', '#footer'],
+            ],
+            $crawler,
+        );
+
+        $this->assertPageStructure(
+            [
+                ['button', 'Voir le fil d\'Ariane'],
+                ['a', 'Accueil', ['href' => '/']],
+                ['a', 'Saisir ou intégrer des arrêtés', ['href' => null]],
+                ['h1', 'Saisir des arrêtés ou intégrer vos données dans DiaLog'],
+                ['h2', 'Saisir un arrêté'],
+                ['h3', 'Créer un compte DiaLog'],
+                ['a', 'Créer un compte', ['href' => '/register']],
+                ['h3', 'Renseigner mes arrêtés'],
+                ['h3', 'Diffuser mes données'],
+                ['h2', 'Intégrer des données depuis votre logiciel de saisie d\'arrêtés'],
+                ['a', 'Nous contacter', ['href' => '/contact']],
+                ['h2', 'Pour en savoir plus'],
+                ['h3', 'Aide en ligne'],
+                ['a', 'Aide en ligne', ['href' => 'https://fabrique-numerique.gitbook.io/doc.dialog.beta.gouv.fr']],
+                ['h2', 'Des questions ?'],
+                ['a', 'Nous contacter', ['href' => '/contact']],
+            ],
+            $crawler,
+        );
+    }
+}
