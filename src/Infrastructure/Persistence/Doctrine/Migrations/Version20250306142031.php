5537a701205d61ffc0cb7cb44910dcca4e3d508e
Lealefoulon <116151499+Lealefoulon@users.noreply.github.com>
2025-03-12 13:54:09 +0100

    Update src/Infrastructure/Persistence/Doctrine/Migrations/Version20250306142031.php
    Co-authored-by: Mathieu MARCHOIS <mathieu.marchois@gmail.com>

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20250306142031.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20250306142031.php
@@ -28,7 +28,6 @@ final class Version20250306142031 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE signing_authority DROP road_name');
         $this->addSql('ALTER TABLE signing_authority DROP city_code');
         $this->addSql('ALTER TABLE signing_authority DROP city_label');
