ca754ee58de241138580bfc17dee82f71dc5fdd2
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2025-04-02 10:59:42 +0200

    Ajout de la date de création d'un feedback (#1260)

--- src/Application/User/Command/SaveFeedbackCommandHandler.php
+++ src/Application/User/Command/SaveFeedbackCommandHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\User\Command;
 
+use App\Application\DateUtilsInterface;
 use App\Application\IdFactoryInterface;
 use App\Domain\User\Feedback;
 use App\Domain\User\Repository\FeedbackRepositoryInterface;
@@ -13,18 +14,20 @@ final class SaveFeedbackCommandHandler
     public function __construct(
         private IdFactoryInterface $idFactory,
         private FeedbackRepositoryInterface $feedbackRepository,
+        private DateUtilsInterface $dateUtils,
     ) {
     }
 
     public function __invoke(SaveFeedbackCommand $command): void
     {
-        $this->feedbackRepository->add(
-            new Feedback(
-                uuid: $this->idFactory->make(),
-                content: $command->content,
-                consentToBeContacted: $command->consentToBeContacted,
-                user: $command->user,
-            ),
+        $feedback = new Feedback(
+            uuid: $this->idFactory->make(),
+            content: $command->content,
+            consentToBeContacted: $command->consentToBeContacted,
+            user: $command->user,
         );
+        $feedback->setCreatedAt($this->dateUtils->getNow());
+
+        $this->feedbackRepository->add($feedback);
     }
 }
