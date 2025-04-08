2d501d608acc57b7323a625bf28287a1128981b7
Lealefoulon <lea.lefoulon@gmail.com>
2025-04-08 14:20:26 +0200

    remplace function en role migration

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20250407142930.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20250407142930.php
@@ -16,7 +16,7 @@ final class Version20250407142930 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE mailing_list (uuid UUID NOT NULL, organization_uuid UUID NOT NULL, name VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, function VARCHAR(150) DEFAULT NULL, PRIMARY KEY(uuid))');
+        $this->addSql('CREATE TABLE mailing_list (uuid UUID NOT NULL, organization_uuid UUID NOT NULL, name VARCHAR(100) NOT NULL, email VARCHAR(255) NOT NULL, role VARCHAR(150) DEFAULT NULL, PRIMARY KEY(uuid))');
         $this->addSql('CREATE INDEX IDX_15C473AFE8766E3B ON mailing_list (organization_uuid)');
         $this->addSql('ALTER TABLE mailing_list ADD CONSTRAINT FK_15C473AFE8766E3B FOREIGN KEY (organization_uuid) REFERENCES organization (uuid) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
