f7fec6cb34d75528c2ec3417432aaf7d8e29943f
Lealefoulon <116151499+Lealefoulon@users.noreply.github.com>
2025-04-08 14:33:38 +0200

    Ajoute la table mailingList dans le modèle de données (#1266)
    * table mailing_list
    
    * tests getters
    
    * modifications after review
    
    * remplace function en role migration

---
+++ tests/Unit/Domain/MailingList/MailingListTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Domain\MailingList;
+
+use App\Domain\Organization\MailingList\MailingList;
+use App\Domain\User\Organization;
+use PHPUnit\Framework\TestCase;
+
+final class MailingListTest extends TestCase
+{
+    public function testGetters(): void
+    {
+        $organization = $this->createMock(Organization::class);
+
+        $mailingList = new MailingList(
+            'e21361a3-999f-45b7-91d1-12cacac3f15c',
+            'Karine Marchand',
+            'email@mairie.gouv.fr',
+            $organization,
+            'Maire',
+        );
+
+        $this->assertSame('e21361a3-999f-45b7-91d1-12cacac3f15c', $mailingList->getUuid());
+        $this->assertSame('Karine Marchand', $mailingList->getName());
+        $this->assertSame('email@mairie.gouv.fr', $mailingList->getEmail());
+        $this->assertSame($organization, $mailingList->getOrganization());
+        $this->assertSame('Maire', $mailingList->getRole());
+    }
+}
