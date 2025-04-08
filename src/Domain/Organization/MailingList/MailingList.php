3109e1ceb81c2e00419f5d74413c977038cdfa83
Lealefoulon <116151499+Lealefoulon@users.noreply.github.com>
2025-04-08 14:33:38 +0200

    Ajoute la table mailingList dans le modèle de données (#1266)
    * table mailing_list
    
    * tests getters
    
    * modifications after review
    
    * remplace function en role migration

---
+++ src/Domain/Organization/MailingList/MailingList.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Organization\MailingList;
+
+use App\Domain\User\Organization;
+
+class MailingList
+{
+    public function __construct(
+        private string $uuid,
+        private string $name,
+        private string $email,
+        private Organization $organization,
+        private ?string $role = null,
+    ) {
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function getOrganization(): Organization
+    {
+        return $this->organization;
+    }
+}
