bfc061d2704248e251463bfb3d377b1dde3d69d0
florimondmanca <florimond.manca@fairness.coop>
2025-05-07 10:20:38 +0200

    Add road_ban_id

---
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20250507081913.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250507081913 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            ALTER TABLE named_street ADD road_ban_id VARCHAR(20) DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_D9113ECE8496CB65 ON named_street (road_ban_id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            DROP INDEX IDX_D9113ECE8496CB65
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE named_street DROP road_ban_id
+        SQL);
+    }
+}
