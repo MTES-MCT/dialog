a8dadcc39ab2e96b978cc69c69b367d275fc2c65
florimondmanca <florimond.manca@fairness.coop>
2025-05-20 16:57:03 +0200

    Add index on voie_nommee.insee_commune

--- src/Infrastructure/Persistence/Doctrine/BdTopoMigrations/Version20250507085352.php
+++ src/Infrastructure/Persistence/Doctrine/BdTopoMigrations/Version20250507085352.php
@@ -20,6 +20,7 @@ final class Version20250507085352 extends AbstractMigration
         $this->addSql('CREATE INDEX IF NOT EXISTS route_numerotee_ou_nommee_numero_gestionnaire_idx ON route_numerotee_ou_nommee (numero, gestionnaire);');
         $this->addSql('CREATE INDEX IF NOT EXISTS point_de_repere_route_numero_gestionnaire_cote_idx ON point_de_repere (route, numero, gestionnaire, cote);');
         $this->addSql('CREATE INDEX voie_nommee_identifiant_voie_ban_idx ON voie_nommee (identifiant_voie_ban)');
+        $this->addSql('CREATE INDEX voie_nommee_insee_commune_idx ON voie_nommee (insee_commune)');
         $this->addSql('CREATE INDEX troncon_de_route_identifiant_voie_ban_gauche_idx ON troncon_de_route (identifiant_voie_ban_gauche)');
         $this->addSql('CREATE EXTENSION IF NOT EXISTS postgis_sfcgal');
         $this->addSql('CREATE INDEX IF NOT EXISTS point_de_repere_identifiant_de_section_idx ON point_de_repere (identifiant_de_section);');
@@ -48,6 +49,7 @@ final class Version20250507085352 extends AbstractMigration
         $this->addSql('DROP INDEX IF EXISTS point_de_repere_identifiant_de_section_idx');
         $this->addSql('DROP EXTENSION IF EXISTS postgis_sfcgal');
         $this->addSql('DROP INDEX IF EXISTS troncon_de_route_identifiant_voie_ban_gauche_idx');
+        $this->addSql('DROP INDEX IF EXISTS voie_nommee_insee_commune_idx');
         $this->addSql('DROP INDEX IF EXISTS voie_nommee_identifiant_voie_ban_idx');
         $this->addSql('DROP INDEX IF EXISTS point_de_repere_route_numero_gestionnaire_cote_idx');
         $this->addSql('DROP INDEX IF EXISTS route_numerotee_ou_nommee_numero_gestionnaire_idx');
