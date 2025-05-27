0d1c533472a3b332d949635bcf540ef24a21bb9a
Lealefoulon <lea.lefoulon@gmail.com>
2025-05-27 17:38:08 +0200

    test getter

--- tests/Unit/Domain/Regulation/StorageRegulationOrderTest.php
+++ tests/Unit/Domain/Regulation/StorageRegulationOrderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Domain\Regulation;
 
+use App\Domain\Regulation\RegulationOrder;
 use App\Domain\Regulation\StorageRegulationOrder;
 use PHPUnit\Framework\TestCase;
 
@@ -11,13 +12,16 @@ final class StorageRegulationOrderTest extends TestCase
 {
     public function testGetters(): void
     {
+        $regulationOrder = $this->createMock(RegulationOrder::class);
         $storageRegulationOrder = (new StorageRegulationOrder(
             uuid: '666a4b2c-55cc-43d1-bb7f-4986f2b2f5f3',
+            regulationOrder: $regulationOrder,
             path: '/path/to/regulationOrder.pdf',
             url: 'https://dialog.oos.cloudgouv-eu-west-1.outscale.com/regulationOrder/666a4b2c-55cc-43d1-bb7f-4986f2b2f5f3/php4fojLb.pdf',
         ));
 
-        $this->assertSame('6598fd41-85cb-42a6-9693-1bc45f4dd392', $storageRegulationOrder->getUuid());
+        $this->assertSame('666a4b2c-55cc-43d1-bb7f-4986f2b2f5f3', $storageRegulationOrder->getUuid());
+        $this->assertSame($regulationOrder, $storageRegulationOrder->getRegulationOrder());
         $this->assertSame('/path/to/regulationOrder.pdf', $storageRegulationOrder->getPath());
         $this->assertSame('https://dialog.oos.cloudgouv-eu-west-1.outscale.com/regulationOrder/666a4b2c-55cc-43d1-bb7f-4986f2b2f5f3/php4fojLb.pdf', $storageRegulationOrder->getUrl());
     }
