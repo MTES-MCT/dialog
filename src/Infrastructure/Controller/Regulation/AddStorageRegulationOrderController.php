4b0514fa311015ce26306d0559e62917ef5ef5c5
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2025-07-11 10:28:18 +0200

    Nelmio config to autorize redirection (#1361)
    * Nelmio config to autorize redirection
    
    * suppression des restrictions 'publiable'
    
    * New try

--- src/Infrastructure/Controller/Regulation/AddStorageRegulationOrderController.php
+++ src/Infrastructure/Controller/Regulation/AddStorageRegulationOrderController.php
@@ -9,12 +9,10 @@ use App\Application\QueryBusInterface;
 use App\Application\Regulation\Command\SaveRegulationOrderStorageCommand;
 use App\Domain\Regulation\Specification\CanOrganizationAccessToRegulation;
 use App\Infrastructure\Form\Regulation\StorageRegulationOrderFormType;
-use App\Infrastructure\Security\Voter\RegulationOrderRecordVoter;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Component\Routing\RouterInterface;
@@ -43,10 +41,6 @@ final class AddStorageRegulationOrderController extends AbstractRegulationContro
     public function __invoke(Request $request, string $uuid): Response
     {
         $regulationOrderRecord = $this->getRegulationOrderRecord($uuid);
-
-        if (!$this->security->isGranted(RegulationOrderRecordVoter::PUBLISH, $regulationOrderRecord)) {
-            throw new AccessDeniedHttpException();
-        }
 
         $regulationOrder = $regulationOrderRecord->getRegulationOrder();
         $command = new SaveRegulationOrderStorageCommand($regulationOrder, null);
