bdfab1dc9ae0cd1c1c9389c3c79f1a6559989385
Lealefoulon <lea.lefoulon@gmail.com>
2025-07-15 12:26:16 +0200

    SendWelcomeEmail

---
+++ src/Application/User/Command/Mail/SendWelcomeEmailCommandHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\User\Command\Mail;
+
+use App\Application\MailerInterface;
+use App\Domain\Mail;
+
+final readonly class SendWelcomeEmailCommandHandler
+{
+    public function __construct(
+        private MailerInterface $mailer,
+    ) {
+    }
+
+    public function __invoke(SendWelcomeEmailCommand $command): void
+    {
+        $this->mailer->send(
+            new Mail(
+                address: $command->email,
+                subject: 'welcome.email.help.subject',
+                template: 'email/user/welcome_user.html.twig',
+                payload: [
+                ],
+            ),
+        );
+    }
+}
