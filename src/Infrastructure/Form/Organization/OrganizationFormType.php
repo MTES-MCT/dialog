62521e2a8580dfd90951fd7ae566fa64d7aa70e1
mmarchois <mathieu.marchois@gmail.com>
2025-07-21 11:08:00 +0200

    Modification des informations de l'organisation

--- src/Infrastructure/Form/Organization/OrganizationFormType.php
+++ src/Infrastructure/Form/Organization/OrganizationFormType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\Form\Organization;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,14 +19,45 @@ final class OrganizationFormType extends AbstractType
                 'name',
                 TextType::class,
                 options: [
-                    'label' => 'organization.name',
+                    'label' => 'organization.form.name',
                 ],
             )
             ->add(
-                'siret',
+                'address',
                 TextType::class,
                 options: [
-                    'label' => 'organization.siret',
+                    'label' => 'organization.establishmentAddress',
+                ],
+            )
+            ->add(
+                'zipCode',
+                TextType::class,
+                options: [
+                    'label' => 'organization.zipCode',
+                ],
+            )
+            ->add(
+                'city',
+                TextType::class,
+                options: [
+                    'label' => 'organization.city',
+                ],
+            )
+            ->add(
+                'addressComplement',
+                TextType::class,
+                options: [
+                    'label' => 'organization.addressComplement',
+                    'required' => false,
+                ],
+            )
+            ->add(
+                'file',
+                FileType::class,
+                options: [
+                    'label' => 'organization.image',
+                    'help' => 'organization.image.help',
+                    'required' => false,
                 ],
             )
             ->add('save', SubmitType::class,
