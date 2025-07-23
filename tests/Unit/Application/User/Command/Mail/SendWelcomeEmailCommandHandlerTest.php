b64be350b9cf906accbfdac20583e3a604c2be9c
Lealefoulon <116151499+Lealefoulon@users.noreply.github.com>
2025-07-23 15:18:51 +0200

    Création d'un email automatique de bienvenue (#1369)
    * SendWelcomeEmail
    
    * tests
    
    * mise en forme email
    
    * emoji
    
    * lien de désinscription
    
    * Revert "lien de désinscription"
    
    This reverts commit 9afc9782eb7cc41a77d6c8eb8deda958ad31e732.
    
    * correction affichage emoji
    
    ---------
    
    Co-authored-by: Mathieu MARCHOIS <mathieu.marchois@gmail.com>

---
+++ tests/Unit/Application/User/Command/Mail/SendWelcomeEmailCommandHandlerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Application\User\Command\Mail;
+
+use App\Application\MailerInterface;
+use App\Application\User\Command\Mail\SendWelcomeEmailCommand;
+use App\Application\User\Command\Mail\SendWelcomeEmailCommandHandler;
+use App\Domain\Mail;
+use PHPUnit\Framework\TestCase;
+
+final class SendWelcomeEmailCommandHandlerTest extends TestCase
+{
+    public function testSendWelcomeEmail(): void
+    {
+        $mailer = $this->createMock(MailerInterface::class);
+        $mailer
+            ->expects(self::once())
+            ->method('send')
+            ->with(
+                $this->equalTo(
+                    new Mail(
+                        address: 'mathieu@fairness.coop',
+                        subject: 'welcome.email.help.subject',
+                        template: 'email/user/welcome_user.html.twig',
+                    ),
+                ),
+            );
+
+        $handler = new SendWelcomeEmailCommandHandler($mailer);
+        $command = new SendWelcomeEmailCommand('mathieu@fairness.coop');
+        ($handler)($command);
+    }
+}
