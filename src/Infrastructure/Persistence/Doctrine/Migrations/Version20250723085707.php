b63412be39435829ed636cfc69d346c7b22c4eab
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2025-07-23 16:02:15 +0200

    Add size to uploaded ressource (#1372)

---
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20250723085707.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250723085707 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE storage_regulation_order ADD file_size INT DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE storage_regulation_order ADD mime_type VARCHAR(255) DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE storage_regulation_order DROP file_size
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE storage_regulation_order DROP mime_type
+        SQL);
+    }
+}
