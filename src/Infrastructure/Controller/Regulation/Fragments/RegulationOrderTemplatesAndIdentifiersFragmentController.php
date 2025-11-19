a8895365db5c344ed8fee33d399c9c0e9b0e1fff
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2025-11-19 20:02:44 +0100

    Corrige la génération d'un identifiant d'arrêté (#1490)

--- src/Infrastructure/Controller/Regulation/Fragments/RegulationOrderTemplatesAndIdentifiersFragmentController.php
+++ src/Infrastructure/Controller/Regulation/Fragments/RegulationOrderTemplatesAndIdentifiersFragmentController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\Controller\Regulation\Fragments;
 
 use App\Application\QueryBusInterface;
+use App\Application\Regulation\Query\GetRegulationOrderIdentifierQuery;
 use App\Application\Regulation\Query\GetRegulationOrderTemplatesQuery;
 use App\Domain\Regulation\DTO\RegulationOrderTemplateDTO;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Turbo\TurboBundle;
 
-final class RegulationOrderTemplatesOptionsFragmentController
+final class RegulationOrderTemplatesAndIdentifiersFragmentController
 {
     public function __construct(
         private \Twig\Environment $twig,
@@ -22,13 +23,15 @@ final class RegulationOrderTemplatesOptionsFragmentController
     }
 
     #[Route(
-        '/_fragment/regulation_order_templates/options',
-        name: 'fragment_regulation_order_templates_options',
+        '/_fragment/regulation_order_templates_and_identifiers',
+        name: 'fragment_regulation_order_templates_and_identifiers',
         methods: ['GET'],
     )]
     public function __invoke(
         Request $request,
         #[MapQueryParameter] string $organizationUuid,
+        #[MapQueryParameter] string $identifierTargetId,
+        #[MapQueryParameter] string $identifierTargetName,
         #[MapQueryParameter] string $targetId,
     ): Response {
         $dto = new RegulationOrderTemplateDTO();
@@ -40,15 +43,20 @@ final class RegulationOrderTemplatesOptionsFragmentController
             $options[$regulationOrderTemplate->uuid] = $regulationOrderTemplate->name;
         }
 
+        $identifier = $this->queryBus->handle(new GetRegulationOrderIdentifierQuery($organizationUuid));
+
         $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
 
         return new Response(
             $this->twig->render(
-                name: 'regulation/fragments/regulation_order_templates_options.stream.html.twig',
+                name: 'regulation/fragments/regulation_order_templates_and_identifiers.stream.html.twig',
                 context: [
                     'options' => $options,
                     'targetId' => $targetId,
                     'organizationUuid' => $organizationUuid,
+                    'identifierTargetId' => $identifierTargetId,
+                    'identifierTargetName' => $identifierTargetName,
+                    'identifierValue' => $identifier,
                 ],
             ),
         );
