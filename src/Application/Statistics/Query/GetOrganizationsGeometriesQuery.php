6fbd35036521c148685bfa051b679fbe33579b01
mmarchois <mathieu.marchois@gmail.com>
2025-12-02 11:20:13 +0100

    Ajout d'une carte de couverture territorial

---
+++ src/Application/Statistics/Query/GetOrganizationsGeometriesQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Statistics\Query;
+
+use App\Application\QueryInterface;
+
+final class GetOrganizationsGeometriesQuery implements QueryInterface
+{
+}
