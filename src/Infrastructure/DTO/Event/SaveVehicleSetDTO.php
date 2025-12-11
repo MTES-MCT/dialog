ce33d8d796a1da3a7ccf8c8fd48107c44c798042
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2025-12-11 14:36:49 +0100

    Corrige le mapping des champs nullable dans l'API (#1556)

--- src/Infrastructure/DTO/Event/SaveVehicleSetDTO.php
+++ src/Infrastructure/DTO/Event/SaveVehicleSetDTO.php
@@ -11,13 +11,13 @@ use Symfony\Component\ObjectMapper\Attribute\Map;
 final class SaveVehicleSetDTO
 {
     public ?bool $allVehicles = null;
-    public ?array $critairTypes = [];
-    public ?array $restrictedTypes = [];
+    public array $critairTypes = [];
+    public array $restrictedTypes = [];
     public ?float $heavyweightMaxWeight = null;
     public ?float $maxWidth = null;
     public ?float $maxLength = null;
     public ?float $maxHeight = null;
     public ?string $otherRestrictedTypeText = null;
-    public ?array $exemptedTypes = [];
+    public array $exemptedTypes = [];
     public ?string $otherExemptedTypeText = null;
 }
