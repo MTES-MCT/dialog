6a4bf6dd6079d4da6772566336735014ae6fca96
Antoine Smagghe <antoine.smagghe.ext@beta.gouv.fr>
2026-01-26 17:46:06 +0100

    feat: introduce alternate regulation type
    Closes #1344

--- src/Domain/Regulation/Enum/MeasureTypeEnum.php
+++ src/Domain/Regulation/Enum/MeasureTypeEnum.php
@@ -6,6 +6,7 @@ namespace App\Domain\Regulation\Enum;
 
 enum MeasureTypeEnum: string
 {
+    case ALTERNATE = 'alternate';
     case NO_ENTRY = 'noEntry';
     case SPEED_LIMITATION = 'speedLimitation';
     case PARKING_PROHIBITED = 'parkingProhibited';
