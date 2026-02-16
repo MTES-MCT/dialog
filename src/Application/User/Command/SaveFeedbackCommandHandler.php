89932eb6e997740a3b008fcf0c7219076a3e7dc8
Antoine Smagghe <44842796+AntoineSmagghe@users.noreply.github.com>
2026-02-16 14:44:51 +0100

    chore: migrate all website contacts to zammad (#1680)

--- src/Application/User/Command/SaveFeedbackCommandHandler.php
+++ src/Application/User/Command/SaveFeedbackCommandHandler.php
@@ -5,16 +5,21 @@ declare(strict_types=1);
 namespace App\Application\User\Command;
 
 use App\Application\DateUtilsInterface;
+use App\Application\Exception\EmailSendingException;
 use App\Application\IdFactoryInterface;
+use App\Application\MailerInterface;
+use App\Domain\Mail;
 use App\Domain\User\Feedback;
 use App\Domain\User\Repository\FeedbackRepositoryInterface;
 
-final class SaveFeedbackCommandHandler
+final readonly class SaveFeedbackCommandHandler
 {
     public function __construct(
         private IdFactoryInterface $idFactory,
         private FeedbackRepositoryInterface $feedbackRepository,
         private DateUtilsInterface $dateUtils,
+        private MailerInterface $mailer,
+        private string $emailSupport,
     ) {
     }
 
@@ -29,5 +34,24 @@ final class SaveFeedbackCommandHandler
         $feedback->setCreatedAt($this->dateUtils->getNow());
 
         $this->feedbackRepository->add($feedback);
+        $this->sendFeedbackByEmail($command);
+    }
+
+    private function sendFeedbackByEmail(SaveFeedbackCommand $command): void
+    {
+        try {
+            $this->mailer->send(new Mail(
+                address: $this->emailSupport,
+                subject: 'contact.email.user_feedback_subject',
+                template: 'email/user/user_feedback.html.twig',
+                payload: [
+                    'content' => $command->content,
+                    'fullName' => $command->user->getFullName(),
+                    'contactEmail' => $command->user->getEmail(),
+                ],
+            ));
+        } catch (\Exception $e) {
+            throw new EmailSendingException('Failed to send feedback by email : ' . $e->getMessage());
+        }
     }
 }
--- tests/Unit/Application/User/Command/SaveFeedbackCommandHandlerTest.php
+++ tests/Unit/Application/User/Command/SaveFeedbackCommandHandlerTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Application\User\Command;
 
 use App\Application\DateUtilsInterface;
+use App\Application\Exception\EmailSendingException;
 use App\Application\IdFactoryInterface;
+use App\Application\MailerInterface;
 use App\Application\User\Command\SaveFeedbackCommand;
 use App\Application\User\Command\SaveFeedbackCommandHandler;
 use App\Domain\User\Feedback;
@@ -48,15 +50,79 @@ final class SaveFeedbackCommandHandlerTest extends TestCase
             ->method('add')
             ->with($this->equalTo($feedback));
 
+        $mailer = $this->createMock(MailerInterface::class);
+        $emailSupport = 'support@example.com';
+
         $handler = new SaveFeedbackCommandHandler(
             $idFactory,
             $feedbackRepository,
             $dateUtils,
+            $mailer,
+            $emailSupport,
         );
         $command = new SaveFeedbackCommand($user);
         $command->content = $content;
         $command->consentToBeContacted = $consentContact;
         $command->user = $user;
+
+        $handler($command);
+    }
+
+    public function testCreateWithMailerException(): void
+    {
+        $content = 'ceci est un avis';
+        $consentContact = false;
+        $idFactory = $this->createMock(IdFactoryInterface::class);
+        $dateUtils = $this->createMock(DateUtilsInterface::class);
+        $feedbackRepository = $this->createMock(FeedbackRepositoryInterface::class);
+        $user = $this->createMock(User::class);
+        $date = new \DateTimeImmutable('2023-01-01 00:00:00');
+
+        $idFactory
+            ->expects(self::once())
+            ->method('make')
+            ->willReturn('0de5692b-cab1-494c-804d-765dc14df674');
+
+        $dateUtils
+            ->expects(self::once())
+            ->method('getNow')
+            ->willReturn($date);
+
+        $feedback = new Feedback(
+            uuid: '0de5692b-cab1-494c-804d-765dc14df674',
+            content: $content,
+            consentToBeContacted: $consentContact,
+            user: $user,
+        );
+        $feedback->setCreatedAt($date);
+
+        $feedbackRepository
+            ->expects(self::once())
+            ->method('add')
+            ->with($this->equalTo($feedback));
+
+        $mailer = $this->createMock(MailerInterface::class);
+        $mailer
+            ->expects(self::once())
+            ->method('send')
+            ->willThrowException(new \Exception('Email service unavailable'));
+
+        $emailSupport = 'support@example.com';
+
+        $handler = new SaveFeedbackCommandHandler(
+            $idFactory,
+            $feedbackRepository,
+            $dateUtils,
+            $mailer,
+            $emailSupport,
+        );
+        $command = new SaveFeedbackCommand($user);
+        $command->content = $content;
+        $command->consentToBeContacted = $consentContact;
+        $command->user = $user;
+
+        $this->expectException(EmailSendingException::class);
+        $this->expectExceptionMessage('Failed to send feedback by email : Email service unavailable');
 
         $handler($command);
     }
