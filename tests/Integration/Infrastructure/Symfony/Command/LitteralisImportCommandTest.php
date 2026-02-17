97c93942407defee2052b1c884b2b1ca1e31fe38
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2026-02-17 11:11:36 +0100

    Englobe literalis dans une transaction pour ne pas supprimer les données si elles n'ont pas été importées (#1686)

--- tests/Integration/Infrastructure/Symfony/Command/LitteralisImportCommandTest.php
+++ tests/Integration/Infrastructure/Symfony/Command/LitteralisImportCommandTest.php
@@ -24,9 +24,11 @@ final class LitteralisImportCommandTest extends KernelTestCase
         $output = $commandTester->getDisplay();
 
         // These results depend on LitteralisMockHttpClient.data.json
+        // Les emprises de type Polygon sont exclues de l'import, d'où 1 seule importée (les 2 autres sont des Polygon).
         $this->assertStringContainsString("Nombre total d'emprises dans Litteralis pour cette organisation : 2", $output);
         $this->assertStringContainsString("Emprises d'intérêt récupérées dans Litteralis : 2", $output);
         $this->assertStringContainsString("Emprises effectivement candidates à l'import : 3 (dans 2 arrêtés)", $output);
-        $this->assertStringContainsString('Emprises importées avec succès : 3 (dans 2 arrêtés)', $output);
+        $this->assertStringContainsString('Emprises importées avec succès : 1 (dans 1 arrêtés)', $output);
+        $this->assertStringContainsString('Emprise ignorée : localisation de type polygon non importée', $output);
     }
 }
