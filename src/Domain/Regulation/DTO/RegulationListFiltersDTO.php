b6f22e3a62699558d5e4d1dfa0c1963023fa405a
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2026-05-04 10:35:47 +0200

    Ajout de tris sur les colonnes de la liste des arrêtés (#1823)

--- src/Domain/Regulation/DTO/RegulationListFiltersDTO.php
+++ src/Domain/Regulation/DTO/RegulationListFiltersDTO.php
@@ -9,11 +9,33 @@ use App\Infrastructure\Security\User\AbstractAuthenticatedUser;
 
 final class RegulationListFiltersDTO
 {
+    public const SORT_IDENTIFIER = 'identifier';
+    public const SORT_PERIOD = 'period';
+    public const SORT_SOURCE = 'source';
+    public const SORT_STATUS = 'status';
+
+    public const SORT_DIR_ASC = 'asc';
+    public const SORT_DIR_DESC = 'desc';
+
+    public const ALLOWED_SORTS = [
+        self::SORT_IDENTIFIER,
+        self::SORT_PERIOD,
+        self::SORT_SOURCE,
+        self::SORT_STATUS,
+    ];
+
+    public const ALLOWED_SORT_DIRS = [
+        self::SORT_DIR_ASC,
+        self::SORT_DIR_DESC,
+    ];
+
     public int $page = Pagination::DEFAULT_PAGE;
     public int $pageSize = Pagination::DEFAULT_PAGE_SIZE;
     public ?string $identifier = null;
     public ?string $organizationUuid = null;
     public ?string $regulationOrderType = null;
     public ?string $status = null;
+    public ?string $sort = null;
+    public ?string $sortDir = null;
     public ?AbstractAuthenticatedUser $user = null;
 }
