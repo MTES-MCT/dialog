0f5a1542204cd1f42cc18612be823b8b892c4826
Antoine Smagghe <antoine.smagghe.ext@beta.gouv.fr>
2026-06-16 10:43:28 +0200

    feat(db): fix tests

--- tests/Unit/Infrastructure/Symfony/Command/AnonymizeDatabaseCommandTest.php
+++ tests/Unit/Infrastructure/Symfony/Command/AnonymizeDatabaseCommandTest.php
@@ -62,7 +62,7 @@ final class AnonymizeDatabaseCommandTest extends TestCase
 
         $executed = [];
         $connection
-            ->expects(self::exactly(8))
+            ->expects(self::exactly(7))
             ->method('executeStatement')
             ->willReturnCallback(function (string $sql, array $params = []) use (&$executed): int {
                 $executed[] = ['sql' => $sql, 'params' => $params];
@@ -84,13 +84,13 @@ final class AnonymizeDatabaseCommandTest extends TestCase
 
         $sqls = array_column($executed, 'sql');
         $this->assertStringContainsString("roles NOT LIKE '%ROLE_SUPER_ADMIN%'", $sqls[0]);
-        $this->assertStringContainsString('UPDATE password_user', $sqls[1]);
-        $this->assertSame(['hash' => 'hashed-password'], $executed[1]['params']);
-        $this->assertStringContainsString('DELETE FROM token', $sqls[2]);
-        $this->assertStringContainsString('DELETE FROM invitation', $sqls[3]);
-        $this->assertStringContainsString('UPDATE feedback', $sqls[4]);
-        $this->assertStringContainsString('UPDATE report_address', $sqls[5]);
-        $this->assertStringContainsString('UPDATE mailing_list', $sqls[6]);
-        $this->assertStringContainsString('UPDATE api_client', $sqls[7]);
+        $this->assertStringContainsString('UPDATE password_user', $sqls[0]);
+        $this->assertSame(['hash' => 'hashed-password'], $executed[0]['params']);
+        $this->assertStringContainsString('DELETE FROM token', $sqls[1]);
+        $this->assertStringContainsString('DELETE FROM invitation', $sqls[2]);
+        $this->assertStringContainsString('UPDATE feedback', $sqls[3]);
+        $this->assertStringContainsString('UPDATE report_address', $sqls[4]);
+        $this->assertStringContainsString('UPDATE mailing_list', $sqls[5]);
+        $this->assertStringContainsString('UPDATE api_client', $sqls[6]);
     }
 }
