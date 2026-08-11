f4fe2a3920b99015076e59f19c5513e6b8a2f8b3
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2026-08-11 16:11:49 +0200

    alerte de modifications non sauvegardées sur les formulaires d'arrêté (#2007)

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -2,10 +2,11 @@ import { startStimulusApp } from '@symfony/stimulus-bridge';
 import { registerTurboEventHandlers } from './lib';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
+// Les fichiers *.test.js sont exclus pour ne pas embarquer les tests (et Vitest) dans le bundle.
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
-    /\.[jt]sx?$/
+    /^(?!.*\.test\.[jt]sx?$).*\.[jt]sx?$/
 ));
 
 // register any custom, 3rd party controllers here
