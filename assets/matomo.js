0ffcf33bc8cace59fb36e0a2bbb00199fc4de52f
Antoine Smagghe <44842796+AntoineSmagghe@users.noreply.github.com>
2026-08-26 10:20:31 +0200

    fix(matomo): avoid sending local data to matomo (#2046)
    Closes #2022

--- assets/matomo.js
+++ assets/matomo.js
@@ -1,16 +1,14 @@
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDoNotTrack", true]);
-_paq.push(["disableCookies"]);
-// sendBeacon permet aux événements envoyés pendant beforeunload (alerte
-// "modifications non sauvegardées") d'aboutir même si la page se ferme.
-_paq.push(["alwaysUseSendBeacon"]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function () {
-    var u = "https://stats.beta.gouv.fr/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '38']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
-})();
+if (process.env.MATOMO_ENABLED === 'true') {
+    var _paq = window._paq = window._paq || [];
+    _paq.push(["disableCookies"]);
+    _paq.push(["alwaysUseSendBeacon"]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+        var u = "https://stats.beta.gouv.fr/";
+        _paq.push(['setTrackerUrl', u + 'matomo.php']);
+        _paq.push(['setSiteId', '38']);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    })();
+}
