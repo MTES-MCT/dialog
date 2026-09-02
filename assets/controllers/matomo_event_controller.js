9d0ad0d8c640c7cdd048df40b0c5df01b060bb0c
Antoine Smagghe <antoine.smagghe.ext@beta.gouv.fr>
2026-09-02 16:38:57 +0200

    feat(matomo): Add matomo events

--- assets/controllers/matomo_event_controller.js
+++ assets/controllers/matomo_event_controller.js
@@ -14,6 +14,16 @@ import { Controller } from '@hotwired/stimulus';
 //
 // The "name" value is optional. See:
 // https://matomo.org/faq/reports/event-tracking-with-matomo/
+//
+// Use the "trackValue" action on a form control (typically a <select>) to
+// send the selected value/label as the event name instead of a static one:
+//
+//   {{ form_row(form.roadType, { row_attr: {
+//       'data-controller': 'matomo-event',
+//       'data-action': 'change->matomo-event#trackValue',
+//       'data-matomo-event-category-value': 'Arrêté',
+//       'data-matomo-event-action-value': 'Sélection type de localisation',
+//   }}) }}
 export default class extends Controller {
     static values = {
         category: String,
@@ -22,6 +32,25 @@ export default class extends Controller {
     };
 
     track() {
+        this.#push(this.nameValue);
+    }
+
+    trackValue(event) {
+        const target = event?.target;
+        let name = this.nameValue;
+
+        if (target) {
+            if (target.tagName === 'SELECT' && target.selectedOptions?.length) {
+                name = target.selectedOptions[0].text.trim();
+            } else if ('value' in target) {
+                name = target.value;
+            }
+        }
+
+        this.#push(name);
+    }
+
+    #push(name) {
         if (!window._paq || !this.categoryValue || !this.actionValue) {
             return;
         }
@@ -30,7 +59,7 @@ export default class extends Controller {
             'trackEvent',
             this.categoryValue,
             this.actionValue,
-            this.nameValue || undefined,
+            name || undefined,
         ]);
     }
 }
